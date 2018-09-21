f42e1b04226b8bcade262fbaa01784e85b142c26
guochunbiao <guochunbiao00416@163.com>
2018-09-21 14:05:21 +0800

    '档案管理缺少文件提交'

---
+++ src/com/nrtest/sea/data/base_app/archivesMan/archivesMan_para.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: 郭春彪
+@license: (C) Copyright 2018, Nari.
+@file: archivesMan_para.py
+@time: 2018/9/17 0017 10:42
+@desc:
+'''
+class ArchivesManData:
+    #基本应用--》档案管理--》
+    para_archivesQuery = '99911400'

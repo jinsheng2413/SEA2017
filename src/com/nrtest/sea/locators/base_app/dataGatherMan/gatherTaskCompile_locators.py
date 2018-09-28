fb4e7844d0440261242082939db8f62668bc50ff
guochunbiao <guochunbiao00416@163.com>
2018-09-28 16:55:22 +0800

    '定制任务管理'

---
+++ src/com/nrtest/sea/locators/base_app/dataGatherMan/gatherTaskCompile_locators.py
@@ -0,0 +1,62 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: 郭春彪
+@license: (C) Copyright 2018, Nari.
+@file: gatherTaskCompile_locators.py
+@time: 2018/9/28 0028 14:53
+@desc:
+'''
+from selenium.webdriver.common.by import By
+
+
+class GatherTaskCompileLocators:
+    # 【查询条件区】
+    # 任务类型
+    QRY_TASK_TYPE = (
+    By.XPATH, "//div[@class=\"x-form-item \"]//label[contains(text(),'任务类型')]/../../div[1]/div[1]//input")
+    QRY_TASK_TYPE_VALUE = (
+    By.XPATH, "//div[@class=\"x-combo-list-inner\"]//*[contains(text(),'负荷类')]/../div[contains(text(),'%s')]")
+
+    # 终端类型
+    QRY_TMAL_TYPE = (
+        By.XPATH, "//div[@class=\"x-form-item \"]//label[contains(text(),'终端类型')]/../../div[1]/div[1]//input")
+    QRY_TMAL_TYPE_VALUE = (
+        By.XPATH, "//div[@class=\"x-combo-list-inner\"]//*[contains(text(),'负荷控制终端')]/../div[%s]")
+
+    # 任务编号
+    QRY_TAST_NO = (
+    By.XPATH, "//div[@class=\"x-form-item \"]//label[contains(text(),'任务编号')]/../../div[1]/div[1]//input")
+
+    # 任务名称
+    QRY_TASK_NAME = (
+    By.XPATH, "//div[@class=\"x-form-item \"]//label[contains(text(),'任务名称')]/../../div[1]/div[1]//input")
+
+    # 任务状态
+    QRY_TASK_STATE = (
+    By.XPATH, "//div[@class=\"x-form-item \"]//label[contains(text(),'任务状态')]/../../div[1]/div[1]//input")
+
+    QRY_TASK_STATE_VALUE = (
+    By.XPATH, "//div[@class=\"x-combo-list-inner\"]//*[contains(text(),'启动')]/../div[contains(text(),'%s')]")
+
+    #终端地址
+    QRY_TMNL_ADDR = (
+        By.XPATH, "//div[@class=\"x-form-item \"]//label[contains(text(),'终端地址')]/../../div[1]/div[1]//input")
+
+    # 采集点名称
+    QRY_COLLECTION_POINT_NAME = (
+        By.XPATH, "//div[@class=\"x-form-item \"]//label[contains(text(),'采集点名称')]/../../div[1]/div[1]//input")
+
+    # 【操作区】
+    BTN_QRY = (By.XPATH,
+               "(//div[@class=\"x-panel-body x-panel-body-noheader x-panel-body-noborder\"]//tbody[@class=\"x-btn-small x-btn-icon-small-left\"]//button[contains(text(),'查询')])[1]")
+
+    # 【js区】
+    # 开始时间，删除readonly属性
+    START_DATE_JS = 'document.getElementById("").removeAttribute("readonly");'
+    # 结束时间，删除readonly属性
+    END_DATE_JS = 'document.getElementById("").removeAttribute("readonly");'
+    JS = "document.getElementsByTagName('input')[0].removeAttribute(\"readOnly\")"
+    # 【显示区】
+    TAB_ONE = (By.XPATH, '(//table[@class=\"x-grid3-row-table\"])[2]')
+    TAB_ONE_VALUE = (By.XPATH, "//*[@class=\"x-grid3-row-table\"])[]//div[]")
---
+++ src/com/nrtest/sea/testcase/base_app/dataGatherMan/test_GatherTaskCompile.py
@@ -0,0 +1,86 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: 郭春彪
+@license: (C) Copyright 2018, Nari.
+@file: test_demo.py
+@time: 2018/9/10 0010 9:21
+@desc:
+'''
+from com.nrtest.sea.data.base_app.dataGatherMan.dataGatherMan_data import DataGatherMan_data
+from com.nrtest.sea.pages.base_app.dataGatherMan.GatherTaskCompile_Page import GatherTaskCompilePage,GatherTaskCompileLocators
+from com.nrtest.sea.task.commonMath import *
+from com.nrtest.common.data_access import DataAccess
+from ddt import ddt, data
+import unittest
+
+
+@ddt
+class TestGatherTaskCompile(unittest.TestCase,GatherTaskCompilePage):
+
+    @classmethod
+    def setUpClass(cls):
+        print("开始执行")
+        # 打开菜单（需要传入对应的菜单编号）
+        cls.driver = openMenu(DataGatherMan_data.gatherTaskCompile_para)
+        clickTabPage('任务查询')
+
+    @classmethod
+    def tearDownClass(cls):
+        print("执行结束")
+        # 刷新浏览器
+        cls.refreshPage(cls)
+
+    def setUp(self):
+        """
+        测试固件的setUp()的代码，主要是测试的前提准备工作
+        :return:
+        """
+
+    def tearDown(self):
+        """
+        测试结束后的操作，这里基本上都是关闭浏览器
+        :return:
+        """
+        # 去除查询干扰数据(要传入对应的page页面类)
+        self.clear_values(GatherTaskCompilePage)
+        # 回收左边树
+        self.recoverLeftTree()
+
+    def query(self, para):
+        '''
+
+        :param para: Dict类型的字典，不是dict
+        ddt实现参数化（tst_case_detail数据表），通过key值，出入对应的值
+        key值要与tst_case_detail表中的XPATH_NAME的值保持一致
+        '''
+
+        #打开左边树并选择
+        self.driver = openLeftTree(para['ORG_NO'])
+        #任务类型
+        self.inputSel_taskType(para['TASK_TYPE'])
+        #任务编号
+        self.inputStr_taskNo(para['TASK_NO'])
+        #任务名称
+        self.inputStr_taskName(para["TASK_NAME"])
+        #任务状态
+        self.inputSel_taskState(para['TASK_STATE'])
+        #终端地址
+        self.inputStr_TMNL_ADDR(para["TMNL_ADDR"])
+        #采集点名称
+        self.inputStr_CollectionPointName(para['COLLECTION_POINT_NAME'])
+        #终端类型
+        self.inputRSel_TmnlType(para["TMNL_TYPE"])
+
+        self.btn_qry()
+        self.sleep_time(2)
+        # 校验
+        result = self.assert_context(*GatherTaskCompileLocators.TAB_ONE)
+        self.assertTrue(result)
+
+    @data(*DataAccess.getCaseData(DataGatherMan_data.gatherTaskCompile_para))
+    def test_query(self, para):
+        self.query(para)
+
+
+

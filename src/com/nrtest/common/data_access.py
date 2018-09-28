13014546bc2d4e56454a604012d5482552d16a9f
oslakesx <oslake@qq.com>
2018-09-28 08:38:00 +0800

    admin用户组测试用例刷新处理

--- src/com/nrtest/common/data_access.py
+++ src/com/nrtest/common/data_access.py
@@ -11,15 +11,12 @@
         :param sql: sql语句
         :return: 返回所有查询数据
 '''
-import cx_Oracle
+
+import os
 
 from com.nrtest.common.dictionary import Dict
 from com.nrtest.common.ora_drv import PyOracle
 from com.nrtest.common.setting import Setting
-
-import os
-
-
 
 os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
@@ -35,30 +32,41 @@ class DataAccess:
         str = pyoracle.callfunc('pkg_nrtest.get_menu_path', 'str', [menuNo])
 
         return str
+
     @staticmethod
     def getLeftTree(treeNO):
         pyoracle = PyOracle.getInstance()
         str = pyoracle.callfunc('pkg_nrtest.get_org_path', 'str', [treeNO])
         return str
+
     @staticmethod
     def getCaseData(menuNo):
         pyoracle = PyOracle.getInstance()
         qry = [Setting.GROUP_USER, menuNo]
         str = pyoracle.callfunc('pkg_nrtest.get_tst_case', 'str', qry)
 
-        #字符串转list
+        # 字符串转list
         rslt = eval(str)
         if (len(rslt) == 0):
             print('请确认以下配置项是否正确：\n1,配置文件（nari_test.conf）的user_group项：%s \n2,菜单编号：%s' % tuple(qry))
         print('当前用例数据：\n', rslt, '\n')
 
-        #dict转Dict ljf
+        # dict转Dict ljf
         for i in range(len(rslt)):
             rslt[i] = Dict(rslt[i])
         return rslt
-if __name__=='__main__':
 
-    str =DataAccess.getCaseData("99911400")
+    @staticmethod
+    def refresh_case(user_no='', group_no=''):
+        para = [Setting.GROUP_USER if user_no == '' else user_no,
+                           '00000' if group_no == '' else group_no]
+        pyoracle = PyOracle.getInstance()
+        str = pyoracle.callproc('pkg_nrtest.refresh_case', para)
+
+        return str
+
+
+if __name__ == '__main__':
+    # DataAccess.refresh_case()
+    str = DataAccess.getCaseData("99911400")
     print(str)
-
-

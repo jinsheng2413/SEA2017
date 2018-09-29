8addd9bcb48226c496ca33c37f5d486d3317f63a
guochunbiao <guochunbiao00416@163.com>
2018-09-29 14:35:05 +0800

    '电价参数管理'

--- src/com/nrtest/sea/data/adv_app/costControlManage/costControlManage_data.py
+++ src/com/nrtest/sea/data/adv_app/costControlManage/costControlManage_data.py
@@ -10,3 +10,5 @@
 class CostControlManage_data:
     # 高级应用--》费控管理--》本地费控--》低压用户购电参数下发
     para_LowUserBuyEleParaGiveOut = '99922120'
+    # 高级应用--》费控管理--》本地费控--》电价参数管理
+    eleParaMan_para = '99922180'

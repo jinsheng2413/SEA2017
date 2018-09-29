02f3a6e367996b87e5e6b355d707a3db965bf7f1
hx <389197447@qq.com>
2018-09-29 16:42:14 +0800

    新模块提交

---
+++ src/com/nrtest/sea/pages/stat_rey/synthQuery/sectDataQuery_page.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: sectDataQuery_page.py
+@time: 2018/9/29 16:09
+@desc:
+'''
+
+from com.nrtest.sea.locators.stat_rey.synthQuery.sectDataQuery_locators import SectDataQueryLocators
+from com.nrtest.common.base_page import Page
+
+# 统计查询→综合查询→抄表段数据查询
+class SectDataQueryPage(Page):
+    # 抄表段编号
+    def inputStr_sect_no(self,content):
+        self.input(content,*SectDataQueryLocators.SECT_NO)
+    # 查询按钮
+    def btn_search(self):
+        self.click(*SectDataQueryLocators.BTN_SEARCH)
+# 数据展示
+    # 查询按钮
+    def btn_tab_search(self):
+        self.click(*SectDataQueryLocators.BTN_TAB_SEARCH)

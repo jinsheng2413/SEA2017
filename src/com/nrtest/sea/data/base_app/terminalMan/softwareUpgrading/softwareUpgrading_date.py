fcf09136a2f35810b7e667e00edf9de4450587b7
hx <389197447@qq.com>
2018-09-29 09:08:31 +0800

    新模块提交

---
+++ src/com/nrtest/sea/data/base_app/terminalMan/softwareUpgrading/softwareUpgrading_date.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: softwareUpgrading_date.py
+@time: 2018/9/28 13:59
+@desc:
+'''
+
+class SoftwareUpgrading_data:
+    # 基本应用→终端管理→软件升级→升级版本管理
+    UpgradeEditionMan_para = '99912310'
+    # 基本应用→终端管理→软件升级→升级版本审批
+    UpgradeEditionApprove_para = '99912320'
+    # 基本应用→终端管理→软件升级→常规零星升级审批
+    RegularSporadicUpgradeApprove_para = '99912340'
+    # 基本应用→终端管理→软件升级→升级任务执行
+    UpgradeTaskExecution_para = '99912360'

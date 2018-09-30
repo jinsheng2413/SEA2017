0e166369e10dca1668fe746cf64dbd33fc0014e0
guochunbiao <guochunbiao00416@163.com>
2018-09-30 09:31:21 +0800

    远程费控执行统计

---
+++ src/com/nrtest/sea/data/adv_app/costControlManage/remoteCustControl/remoteCustControl_data.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: 郭春彪
+@license: (C) Copyright 2018, Nari.
+@file: remoteCustControl_data.py
+@time: 2018/9/29 0029 15:40
+@desc:
+'''
+class RemoteCustControl_data:
+    #高级应用--》费控管理--》远程费控--》远程费控执行统计
+    prePaidStatus_para = '99922230'
+    Tab_One = '按指令执行统计'
+    Tab_Two = '按用户执行统计'

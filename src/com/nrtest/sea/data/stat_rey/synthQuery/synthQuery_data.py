a3f9517bbf866b4c4678032d4a744b7b6f13d0d3
hx <389197447@qq.com>
2018-10-08 11:22:13 +0800

    全事件电表事件查询

--- src/com/nrtest/sea/data/stat_rey/synthQuery/synthQuery_data.py
+++ src/com/nrtest/sea/data/stat_rey/synthQuery/synthQuery_data.py
@@ -15,3 +15,5 @@ class SynthQuery_data:
     SectDataQuery_para = '99941600'
     # 统计查询→综合查询→批量数据查询
     BatchDataQuery_para = '99941700'
+    #统计查询→综合查询→全事件电表事件查询
+    AllEventMeterEventQuery_para = '99941702'
--- src/com/nrtest/sea/testcase/stat_rey/synthQuery/test_allEventMeterEventQuery.py
+++ src/com/nrtest/sea/testcase/stat_rey/synthQuery/test_allEventMeterEventQuery.py
@@ -3,27 +3,26 @@
 '''
 @author: 韩笑
 @license: (C) Copyright 2018, Nari.
-@file: test_batchDataQuery.py
-@time: 2018/9/30 10:21
+@file: test_allEventMeterEventQuery.py
+@time: 2018/9/30 14:43
 @desc:
 '''
 
 import unittest
 from com.nrtest.common.data_access import DataAccess
-from com.nrtest.sea.pages.stat_rey.synthQuery.batchDataQuery_page import BatchDataQueryPage
+from com.nrtest.sea.pages.stat_rey.synthQuery.allEventMeterEventQuery_page import AllEventMeterEventQueryPage
 from com.nrtest.sea.data.stat_rey.synthQuery.synthQuery_data import SynthQuery_data
 from com.nrtest.sea.task.commonMath import *
 import ddt
 
-
-# 统计查询→综合查询→批量数据查询
+# 统计查询→综合查询→全事件电表事件查询
 @ddt.ddt
-class TestBatchDataQuery(unittest.TestCase,BatchDataQueryPage):
+class TestAllEventMeterEventQuery(unittest.TestCase,AllEventMeterEventQueryPage):
     @classmethod
     def setUpClass(cls):
         print("开始执行")
         # 打开菜单（需要传入对应的菜单编号）
-        cls.driver = openMenu(SynthQuery_data.BatchDataQuery_para)
+        cls.driver = openMenu(SynthQuery_data.AllEventMeterEventQuery_para)
 
     @classmethod
     def tearDownClass(cls):
@@ -48,17 +47,19 @@ class TestBatchDataQuery(unittest.TestCase,BatchDataQueryPage):
     def query(self, para):
         #打开左边树并选择
         self.driver = openLeftTree(para['TREE_ORG_NO'])
-        #日期
-        self.inputDt_date(para['DATA'])
-        #终端资产号
-        self.inputStr_tmnl_asset_no(para['TMNL_ASSET_NO'])
-        #用户类型
-        self.inputCSel_cons_type(para['CONS_TYPE'])
-        #终端地址
-        self.inputStr_tmnl_addr(para['TMNL_ADDR'])
+        #电表资产号
+        self.inputStr_meter_asset_no(para['METER_ASSET_NO'])
+        # 事件等级
+        self.inputSel_event_level(para['EVENT_LEVEL'])
+        # 事件类型
+        self.inputSel_event_type(para['EVENT_TYPE'])
+        # 采集开始时间
+        self.inputDt_start_date(para['START_DATE'])
+        # 采集结束时间
+        self.inputDt_end_date(para['END_DATE'])
         #查询按钮
         self.btn_search()
 
-    @ddt.data(*DataAccess.getCaseData(SynthQuery_data.BatchDataQuery_para))
+    @ddt.data(*DataAccess.getCaseData(SynthQuery_data.AllEventMeterEventQuery_para))
     def test_der(self, para):
         self.query(para)

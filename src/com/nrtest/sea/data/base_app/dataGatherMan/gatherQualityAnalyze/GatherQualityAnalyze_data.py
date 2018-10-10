ef483193f203ff5726041f2552064f81a4b64fa4
guochunbiao <guochunbiao00416@163.com>
2018-10-10 16:47:44 +0800

    '采集完整率菜单提交'

--- src/com/nrtest/sea/data/base_app/dataGatherMan/gatherQualityAnalyze/GatherQualityAnalyze_data.py
+++ src/com/nrtest/sea/data/base_app/dataGatherMan/gatherQualityAnalyze/GatherQualityAnalyze_data.py
@@ -3,14 +3,14 @@
 '''
 @author: 韩笑
 @license: (C) Copyright 2018, Nari.
-@file: gatherQualityAnalyze_data.py
+@file: GatherQualityAnalyze_data.py
 @time: 2018/9/17 16:36
 @desc:
 '''
 
 
 
-class gatherQualityAnalyze_data:
+class GatherQualityAnalyze_data:
     # 基本应用→数据采集管理→采集质量分析→采集成功率
     para_GatherSuccessRate = '99913401'
     # 基本应用→数据采集管理→采集质量分析→事件记录结果统计
@@ -19,3 +19,7 @@ class gatherQualityAnalyze_data:
     collectSuccessRateStat_para = '99913270'
     # 基本应用→数据采集管理→采集质量分析→采集成功率(冀北)
     collectSuccessRateJb_para ='99913260'
+    # 基本应用→数据采集管理→采集质量分析→采集完整率
+    readCompleteRate_para = '99913220'
+    readCompleteRate_tab = '采集完整率'
+    readCompleteRateCount_tab = '采集完整率统计'

9ca3f65c7dfb576c9dda46fbbb0f7bd3bb19fc16
oslakesx <oslake@qq.com>
2018-10-24 21:39:12 +0800

    修正按菜单名称定位

--- src/com/nrtest/sea/pages/other/menu_page.py
+++ src/com/nrtest/sea/pages/other/menu_page.py
@@ -8,26 +8,34 @@
 @desc:
 '''
 
-from com.nrtest.sea.locators.other.menu_locators import MenuLocators
 from com.nrtest.common.base_page import Page
 from com.nrtest.common.data_access import DataAccess
-from com.nrtest.sea.task.login import Login
 from com.nrtest.common.setting import Setting
-
 from com.nrtest.sea.data.base_app.archivesMan.archivesAnalysisOfAnomaly_para import ArchivesAnalysisOfAnomaly_para
+from com.nrtest.sea.locators.other.menu_locators import MenuLocators
+from com.nrtest.sea.task.login import Login
+
+
 class MenuPage(Page):
-    #click_menu(ArchivesAnalysisOfAnomaly_para(1))
+    # click_menu(ArchivesAnalysisOfAnomaly_para(1))
 
-
-    def click_menu(self,menu_no):
+    def click_menu(self, menu_no, by_name=False):
         """
         定位级菜单element,并调用Base_Page类的click方法选择级菜单
-        :param xpath:菜单xpath序号，如：1;1;2  一级菜单下第一个菜单下的第二个子菜单
-        :return:
+        :param menu_no: 菜单编号
+        :param by_name: True-按菜单名定位；False-按菜单坐标定位
+        :return: 返回driver
         """
 
-        kl = DataAccess()
-        coordinate = kl.getMenu(menu_no)
+        #按菜单名定位菜单
+        if (by_name):
+            self.click_menu_by_name(menu_no);
+            return self.driver
+
+        # 静态函数可以直接调用，不需要实例化
+        # kl = DataAccess()
+        # coordinate = kl.getMenu(menu_no)
+        coordinate = DataAccess.getMenu(menu_no)
 
         items = coordinate.split(';')
         l = len(items)
@@ -38,7 +46,30 @@ class MenuPage(Page):
             idx = idx + 1 if i == 0 else idx
             loc = (locators[0], locators[1] % idx)
             if (l == 4 and i == 2) or (l == 5 and i in (2, 3)):
-                pg = Page
+                # pg = Page  #无效代码  ljf  2018-10-24
+                self.hover(*loc)
+            else:
+                self.click(*loc)
+        return self.driver
+
+    def click_menu_by_name(self, menu_no):
+        """
+        定位级菜单element,并调用Base_Page类的click方法选择级菜单
+        :param menu_no: 菜单编号
+        :return:各级菜单名，如：基本应用;档案管理;档案同步  一级菜单下第一个菜单下的第二个子菜单
+        """
+
+        # 静态函数可以直接调用，不需要实例化
+        # kl = DataAccess()
+        # coordinate = kl.getMenu(menu_no)
+        coordinate = DataAccess.getMenu(menu_no)
+
+        items = coordinate.split(';')
+        l = len(items)
+        for i in range(len(items)):
+            locators = getattr(MenuLocators, 'MENU_LEVEL' + str(i + 1))
+            loc = (locators[0], locators[1] % items[i])
+            if (l == 4 and i == 2) or (l == 5 and i in (2, 3)):
                 self.hover(*loc)
             else:
                 self.click(*loc)
@@ -46,9 +77,8 @@ class MenuPage(Page):
 
     # 左边树
     def btn_plus(self, index):
-      locator = self.get_select_locator(MenuLocators.BTN_LEFT_PLUS, index)
-      self.click(*locator)
-
+        locator = self.get_select_locator(MenuLocators.BTN_LEFT_PLUS, index)
+        self.click(*locator)
 
     def btn_company_plus(self, index):
         locator = self.get_select_locator(MenuLocators.BTN_COMPANY_PLUS, index)
@@ -57,15 +87,15 @@ class MenuPage(Page):
 
     # 选中省份
     def btn_select_province(self):
-     self.click(*MenuLocators.BTN_LEFT_MENU_ELETRIC)
+        self.click(*MenuLocators.BTN_LEFT_MENU_ELETRIC)
 
     # 选择公司
     def btn_select_company(self, number):
-       nb = number + 1
-       lr = self.get_select_locator(MenuLocators.BTN_COMPANY, nb)
-       self.click(*lr)
+        nb = number + 1
+        lr = self.get_select_locator(MenuLocators.BTN_COMPANY, nb)
+        self.click(*lr)
 
-       # 点击双向箭头
+        # 点击双向箭头
 
     def btn_left_arrow(self):
         self.click(*MenuLocators.BTN_LEFT_MENU)
@@ -82,7 +112,7 @@ class MenuPage(Page):
 
     # 选择所
 
-    def btn_select_user(self,index):
+    def btn_select_user(self, index):
         lr = self.get_select_locator(MenuLocators.BTN_COUNTY_AND_USER, index)
         self.click(*lr)
 
@@ -102,8 +132,7 @@ class MenuPage(Page):
         kl = DataAccess()
         tree = kl.getLeftTree(tree_no)
 
-
-        #self.btn_suitable_arrow()
+        # self.btn_suitable_arrow()
         items = tree.split(';')
         l = len(items)
 
@@ -111,40 +140,33 @@ class MenuPage(Page):
             self.btn_plus(1)
             self.btn_select_company(int(items[1]))
             print(int(items[1]))
-        elif l in (3,4):
+        elif l in (3, 4):
             self.btn_plus(1)
             if int(items[1]) is 5:
                 self.btn_plus(1)
             else:
-              self.btn_company_plus(items[1])
+                self.btn_company_plus(items[1])
             if l is 3:
-                self.btn_select_county(int(items[2])+1)
+                self.btn_select_county(int(items[2]) + 1)
             else:
-                self.btn_company_plus(int(items[2])+int(items[1]))
-                self.btn_select_user(int(items[3])+1)
+                self.btn_company_plus(int(items[2]) + int(items[1]))
+                self.btn_select_user(int(items[3]) + 1)
         elif l is 1:
             self.btn_select_province()
 
         return self.driver
 
-
-    def clickLeftTree(self,tree):
+    def clickLeftTree(self, tree):
         tree = tree.split(';')
         self.btn_plus(1)
         self.btn_company_plus(list[0])
         self.btn_company_plus(list[1])
 
-
-
     def getAssert(self, num):
-            self.get_select_locator(MenuLocators.TAB_ONE, num)
+        self.get_select_locator(MenuLocators.TAB_ONE, num)
 
     def getSecondAssert(self, num):
-            self.get_select_locator(MenuLocators.TAB_TWO, num)
-
-
-
-
+        self.get_select_locator(MenuLocators.TAB_TWO, num)
 
 
 if __name__ == '__main__':

a5afea90ec20edbba048e3113ad4ec592c9a5d64
hx <389197447@qq.com>
2018-10-30 15:26:21 +0800

    高级应用→线损分析→线损统计分析→台区线损分析
    Signed-off-by: hx <389197447@qq.com>

---
+++ src/com/nrtest/sea/data/adv_app/lineLossAnalysis/lineLossStatisticsAnalysis/lineLossStatisticsAnalysis_data.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: lineLossStatisticsAnalysis_data.py
+@time: 2018/10/30 13:31
+@desc:
+"""
+
+
+class LineLossStatisticsAnalysis_data:
+    # 高级应用→线损分析→线损统计分析→台区线损分析
+    TgLineLossAnalysis_para = '99924210'

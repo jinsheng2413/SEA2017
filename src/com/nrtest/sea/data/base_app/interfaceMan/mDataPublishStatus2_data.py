8bf66606132a999f72eb9f7db13dc4c728e57709
js <1234567@qq.com>
2018-10-30 16:53:22 +0800

    基本应用--接口管理--营销业务接口--数据发布状态

---
+++ src/com/nrtest/sea/data/base_app/interfaceMan/mDataPublishStatus2_data.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: jinsheng
+@license: (C) Copyright 2018, Nari.
+@file: mDataPublishStatus2_data.py
+@time: 2018-10-30 15:38
+@desc:
+'''
+
+
+class InterfaceManager_data:
+    # 基本应用--接口管理--营销业务接口--数据发布情况
+    para_MDataPublishStatus2 = '99914110'
---
+++ src/com/nrtest/sea/pages/base_app/interfaceMan/mDataPublishStatus2_page.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: jinsheng
+@license: (C) Copyright 2018, Nari.
+@file: mDataPublishStatus2_page.py
+@time: 2018-10-30 16:03
+@desc:
+'''
+
+from com.nrtest.common.base_page import Page
+from com.nrtest.sea.locators.base_app.interfaceMan.mDataPublishStatus2_locators import MDataPublishStatus2_locators
+
+
+class MDataPublishStatus2Page(Page):
+    # 业务系统
+    def inputSel_BusinessSystem(self, index):
+        self.click(*MDataPublishStatus2_locators.QRY_BUSINESS_SYSTEM)
+        locator = self.get_select_locator(MDataPublishStatus2_locators.QRY_BUSINESS_SYSTEM_VALUE, index)
+        self.click(*locator)
+
+    # 发布时间 开始
+    def inputStr_receive_time(self, value):
+        self.input(value, *MDataPublishStatus2_locators.QRY_DATE_BEGIN)
+
+    # 结束时间
+    def inputStr_end_time(self, value):
+        self.input(value, *MDataPublishStatus2_locators.QRY_DATE_END)
+
+    # 查询
+    def btn_qry(self):
+        self.click(*MDataPublishStatus2_locators.BTN_QUERY)

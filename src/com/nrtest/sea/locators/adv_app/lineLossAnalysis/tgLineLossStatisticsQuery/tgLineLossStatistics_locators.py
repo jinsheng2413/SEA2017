c82c8b96c8aa1835f9738b15e0d1bb4b043c49d6
hx <389197447@qq.com>
2018-11-01 17:06:30 +0800

    高级应用→线损分析→台区线损统计查询→台区线损统计
    Signed-off-by: hx <389197447@qq.com>

---
+++ src/com/nrtest/sea/locators/adv_app/lineLossAnalysis/tgLineLossStatisticsQuery/tgLineLossStatistics_locators.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: tgLineLossStatistics_locators.py
+@time: 2018/11/1 16:29
+@desc:
+"""
+
+from selenium.webdriver.common.by import By
+
+
+# 高级应用→线损分析→台区线损统计查询→台区线损统计
+class TgLineLossStatisticsLocators:
+    # 线损维度
+    QRY_LINE_LOSS_DIMENSION = (By.XPATH, '//label[contains(text(),"线损维度")]/../div/div/img')
+    QRY_LINE_LOSS_DIMENSION_VALUE = (By.XPATH, '//div[@class="x-combo-list-inner"]/div[%s]')
+    # 日期
+    QRY_DATE = (By.XPATH, '//label[contains(text(),"日期")]/../div/div/input')
+    # 查询按钮
+    BTN_SEARCH = (By.XPATH, '//button[text()="查询"]')
+
+    # 【JS属性】
+    # 查询日期，删除readonly属性
+    DATE_JS = 'document.getElementsByTagName("input")[6].removeAttribute("readonly");'
---
+++ src/com/nrtest/sea/pages/adv_app/lineLossAnalysis/tgLineLossStatisticsQuery/tgLineLossStatistics_page.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: tgLineLossStatistics_page.py
+@time: 2018/11/1 16:40
+@desc:
+"""
+
+from com.nrtest.common.base_page import Page
+from com.nrtest.sea.locators.adv_app.lineLossAnalysis.tgLineLossStatisticsQuery.tgLineLossStatistics_locators import \
+    TgLineLossStatisticsLocators
+
+
+# 高级应用→线损分析→台区线损统计查询→台区线损统计
+class TgLineLossStatisticsPage(Page):
+    # 线损维度
+    def inputSel_line_loss_dimension(self, index):
+        self.click(*TgLineLossStatisticsLocators.QRY_LINE_LOSS_DIMENSION)
+        locator = self.get_select_locator(TgLineLossStatisticsLocators.QRY_LINE_LOSS_DIMENSION_VALUE, index)
+        self.click(*locator)
+
+    # 查询日期
+    def inputDt_date(self, content):
+        self.exec_script(TgLineLossStatisticsLocators.DATE_JS)
+        self.input(content, *TgLineLossStatisticsLocators.QRY_DATE)
+
+    # 查询按钮
+    def btn_search(self):
+        self.click(*TgLineLossStatisticsLocators.BTN_SEARCH)

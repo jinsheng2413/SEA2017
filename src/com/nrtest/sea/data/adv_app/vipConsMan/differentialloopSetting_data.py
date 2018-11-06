123896d96dc79dec661b6ab17623d5a8fa3f6801
js <1234567@qq.com>
2018-11-06 13:59:09 +0800

    高级应用--重点用户监测--加载防窃电设备 新增

---
+++ src/com/nrtest/sea/data/adv_app/vipConsMan/differentialloopSetting_data.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: jinsheng
+@license: (C) Copyright 2018, Nari.
+@file: differentialloopSetting_data.py
+@time: 2018-11-06 9:14
+@desc:
+'''
+
+
+class VipConsMan:
+    # 高级应用--重点用户监测--差动回路设置
+    para_differentialloopSetting = '99926600'
---
+++ src/com/nrtest/sea/pages/adv_app/vipConsMan/differentialloopSetting_page.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+
+'''
+@author: jinsheng
+@license: (C) Copyright 2018, Nari.
+@file: differentialloopSetting_page.py
+@time: 2018-11-06 10:50
+@desc:
+'''
+
+from com.nrtest.common.base_page import Page
+from com.nrtest.sea.locators.adv_app.vipConsMan.differentialloopSetting_locators import DifferentialloopSetting_locators
+
+
+class DifferentialloopSetting_Page(Page):
+    # 用户编号
+    def inputStr_cons_no(self, value):
+        self.input(value, *DifferentialloopSetting_locators.QRY_CONS_NO)
+
+    # 用户名称
+    def inputStr_cons_name(self, value):
+        self.input(value, *DifferentialloopSetting_locators.QRY_CONS_NAME)
+
+    # 终端地址
+    def inputStr_tmnl_addr(self, value):
+        self.input(value, *DifferentialloopSetting_locators.QRY_TMNL_ADDR)
+
+    # 终端资产号
+    def inputStr_tmnl_asst_no(self, value):
+        self.input(value, *DifferentialloopSetting_locators.QRY_TMNL_ASST_NO)
+
+    # 查询
+    def btn_qry(self):
+        self.click(*DifferentialloopSetting_locators.BTN_QUERY)

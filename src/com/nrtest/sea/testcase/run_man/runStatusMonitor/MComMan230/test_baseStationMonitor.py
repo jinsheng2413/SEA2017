ba395fc77c111e33ae342d47eeed9af8d2d290bb
guochunbiao <guochunbiao00416@163.com>
2018-11-07 13:54:16 +0800

    # 运行管理--》采集信道管理--》230M通信管理--》站点状态监控

--- src/com/nrtest/sea/testcase/run_man/runStatusMonitor/MComMan230/test_baseStationMonitor.py
+++ src/com/nrtest/sea/testcase/run_man/runStatusMonitor/MComMan230/test_baseStationMonitor.py
@@ -20,31 +20,22 @@ from com.nrtest.sea.pages.run_man.runStatusMonitor.MComMan230.baseStationManage_
     BaseStationManageLocators
 from com.nrtest.sea.task.commonMath import *
 
-#运行管理--》采集信道管理--》230M通信管理--》站点状态监控
+
 @ddt
-class TestbaseStationManage(unittest.TestCase, BaseStationManagePage):
+class TestDemo(unittest.TestCase, BaseStationManagePage):
 
     @classmethod
     def setUpClass(cls):
         print("开始执行")
         # 打开菜单（需要传入对应的菜单编号）
 
-        cls.driver = openMenu(MComMan230.baseStationManage_para)
+        cls.driver = openMenu(MComMan230.baseStationMonitor_para)
 
     @classmethod
     def tearDownClass(cls):
         print("执行结束")
         # 关闭菜单页面
         cls.closePages(cls)
-
-    def tearDown(self):
-        """
-        测试结束后的操作，这里基本上都是关闭浏览器
-        :return:
-        """
-
-        # 回收左边树
-        self.recoverLeftTree()
 
     def query(self, para):
         '''

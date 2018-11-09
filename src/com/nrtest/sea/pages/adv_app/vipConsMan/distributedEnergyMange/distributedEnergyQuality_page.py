a2024c898fc3a6c3fe4294dff5535c6e184dd586
hx <389197447@qq.com>
2018-11-09 14:40:06 +0800

    高级应用→重点用户监测→分布式电源管理→分布式电源采集质量
    Signed-off-by: hx <389197447@qq.com>

---
+++ src/com/nrtest/sea/pages/adv_app/vipConsMan/distributedEnergyMange/distributedEnergyQuality_page.py
@@ -0,0 +1,125 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: distributedEnergyQuality_page.py
+@time: 2018/11/9 11:30
+@desc:
+"""
+
+from com.nrtest.common.base_page import Page
+from com.nrtest.sea.locators.adv_app.vipConsMan.distributedEnergyMange.distributedEnergyQuality_locators import *
+
+
+# 高级应用→重点用户监测→分布式电源管理→分布式电源采集质量→采集成功率趋势
+class SuccessRateTrendPage(Page):
+    # 日期
+    def inputDt_date(self, content):
+        self.exec_script(SuccessRateTrendLocators.DATE_JS)
+        self.input(content, *SuccessRateTrendLocators.QRY_DATE)
+
+    # 本地通讯方式
+    def inputSel_comm_mode(self, index):
+        self.click(*SuccessRateTrendLocators.QRY_COMM_MODE)
+        locator = self.get_select_locator(
+            SuccessRateTrendLocators.QRY_COMM_MODE_VALUE, index)
+        self.click(*locator)
+
+    # 远程通讯方式
+    def inputSel_coll_mode(self, index):
+        self.click(*SuccessRateTrendLocators.QRY_COLL_MODE)
+        locator = self.get_select_locator(
+            SuccessRateTrendLocators.QRY_COLL_MODE_VALUE, index)
+        self.click(*locator)
+
+    # 发电量消纳方式
+    def inputSel_abso_type(self, index):
+        self.click(*SuccessRateTrendLocators.QRY_ABSO_MODE)
+        locator = self.get_select_locator(
+            SuccessRateTrendLocators.QRY_ABSO_MODE_VALUE, index)
+        self.click(*locator)
+
+    # 发电类型
+    def inputSel_elec_type(self, index):
+        self.click(*SuccessRateTrendLocators.QRY_ELEC_TYPE)
+        locator = self.get_select_locator(
+            SuccessRateTrendLocators.QRY_ELEC_TYPE_VALUE, index)
+        self.click(*locator)
+
+    # 查询按钮
+    def btn_search(self):
+        self.click(*SuccessRateTrendLocators.BTN_SEARCH)
+
+
+# 高级应用→重点用户监测→分布式电源管理→分布式电源采集质量→采集成功率统计
+class SuccessRateStatisticsPage(Page):
+    # 日期
+    def inputDt_date(self, content):
+        self.exec_script(SuccessRateStatisticsLocators.DATE_JS)
+        self.input(content, *SuccessRateStatisticsLocators.QRY_DATE)
+
+    # 本地通讯方式
+    def inputSel_comm_mode(self, index):
+        self.click(*SuccessRateStatisticsLocators.QRY_COMM_MODE)
+        locator = self.get_select_locator(
+            SuccessRateStatisticsLocators.QRY_COMM_MODE_VALUE, index)
+        self.click(*locator)
+
+    # 远程通讯方式
+    def inputSel_coll_mode(self, index):
+        self.click(*SuccessRateStatisticsLocators.QRY_COLL_MODE)
+        locator = self.get_select_locator(
+            SuccessRateStatisticsLocators.QRY_COLL_MODE_VALUE, index)
+        self.click(*locator)
+
+    # 发电量消纳方式
+    def inputSel_abso_type(self, index):
+        self.click(*SuccessRateStatisticsLocators.QRY_ABSO_MODE)
+        locator = self.get_select_locator(
+            SuccessRateStatisticsLocators.QRY_ABSO_MODE_VALUE, index)
+        self.click(*locator)
+
+    # 发电类型
+    def inputSel_elec_type(self, index):
+        self.click(*SuccessRateStatisticsLocators.QRY_ELEC_TYPE)
+        locator = self.get_select_locator(
+            SuccessRateStatisticsLocators.QRY_ELEC_TYPE_VALUE, index)
+        self.click(*locator)
+
+    # 查询按钮
+    def btn_search(self):
+        self.click(*SuccessRateStatisticsLocators.BTN_SEARCH)
+
+
+# 高级应用→重点用户监测→分布式电源管理→分布式电源采集质量→采集成功率明细
+class SuccessRateDetailPage(Page):
+    # 电能表用途
+    def inputSel_meter_purpose(self, index):
+        self.click(*SuccessRateDetailLocators.QRY_METER_PURPOSE)
+        locator = self.get_select_locator(
+            SuccessRateDetailLocators.QRY_METER_PURPOSE_VALUE, index)
+        self.click(*locator)
+
+    # 发电类型
+    def inputSel_elec_type(self, index):
+        self.click(*SuccessRateDetailLocators.QRY_ELEC_TYPE)
+        locator = self.get_select_locator(
+            SuccessRateDetailLocators.QRY_ELEC_TYPE_VALUE, index)
+        self.click(*locator)
+
+    # 发电量消纳方式
+    def inputSel_abso_type(self, index):
+        self.click(*SuccessRateDetailLocators.QRY_ABSO_MODE)
+        locator = self.get_select_locator(
+            SuccessRateDetailLocators.QRY_ABSO_MODE_VALUE, index)
+        self.click(*locator)
+
+    # 日期
+    def inputDt_date(self, content):
+        self.exec_script(SuccessRateDetailLocators.DATE_JS)
+        self.input(content, *SuccessRateDetailLocators.QRY_DATE)
+
+    # 查询按钮
+    def btn_search(self):
+        self.click(*SuccessRateDetailLocators.BTN_SEARCH)
---
+++ src/com/nrtest/sea/testcase/adv_app/vipConsMan/distributedEnergyMange/test_successRateTrend.py
@@ -0,0 +1,73 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: test_successRateTrend.py
+@time: 2018/11/9 11:41
+@desc:
+"""
+
+import unittest
+
+from ddt import ddt, data
+
+from com.nrtest.common.BeautifulReport import BeautifulReport
+from com.nrtest.common.data_access import DataAccess
+from com.nrtest.sea.data.adv_app.vipConsMan.distributedEnergyMange.distributedEnergyManage_data import \
+    DistributedEnergyMange_data
+from com.nrtest.sea.pages.adv_app.vipConsMan.distributedEnergyMange.distributedEnergyQuality_page import \
+    SuccessRateTrendPage
+from com.nrtest.sea.task.commonMath import *
+
+
+# 高级应用→重点用户监测→分布式电源管理→分布式电源采集质量→采集成功率趋势
+@ddt
+class TestSuccessRateTrend(unittest.TestCase, SuccessRateTrendPage):
+    @classmethod
+    def setUpClass(cls):
+        print('开始执行')
+        # 打开菜单（需要传入对应的菜单编号）
+        cls.driver = openMenu(
+            DistributedEnergyMange_data.DistributedEnergyQuality_para, True)
+
+    @classmethod
+    def tearDownClass(cls):
+        print('执行结束')
+        # 关闭菜单页面
+        cls.closePages(cls)
+
+    def setUp(self):
+        """
+        测试固件的setUp()的代码，主要是测试的前提准备工作
+        :return:
+        """
+
+    def tearDown(self):
+        """
+        测试结束后的操作，这里基本上都是关闭浏览器
+        :return:
+        """
+        # 回收左边树
+        self.recoverLeftTree()
+
+    def query(self, para):
+        # 打开左边树并选择
+        self.driver = openLeftTree(para['TREE_ORG_NO'])
+        # 日期
+        self.inputDt_date(para['DATE'])
+        # 本地通讯方式
+        self.inputSel_comm_mode(para['COMM_MODE'])
+        # 远程通讯方式
+        self.inputSel_coll_mode(para['COLL_MODE'])
+        # 发电量消纳方式
+        self.inputSel_abso_type(para['ABSO_TYPE'])
+        # 发电类型
+        self.inputSel_elec_type(para['ELEC_TYPE'])
+        # 查询按钮
+        self.btn_search()
+
+    @BeautifulReport.add_test_img()
+    @data(*DataAccess.getCaseData(DistributedEnergyMange_data.DistributedEnergyQuality_para, tabName='采集成功率趋势'))
+    def test_der(self, para):
+        self.query(para)

ee73c4229877dba28657acb54a590faa24bd7f28
guochunbiao <guochunbiao00416@163.com>
2018-11-20 11:16:55 +0800

    # 系统管理--》档案核查管理--》考核减免汇总

--- src/com/nrtest/sea/data/sys_mam/archivesVerficationMan/archivesVerficationMan_data.py
+++ src/com/nrtest/sea/data/sys_mam/archivesVerficationMan/archivesVerficationMan_data.py
@@ -26,3 +26,5 @@ class ArchivesVerficationMan_data:
     taskTemplateSet_para = '99959700'
     # 系统管理--》档案核查管理--》考核减免申请
     checkReduceApplication_para = '99959900'
+    # 系统管理--》档案核查管理--》考核减免汇总
+    checkReducePool_para = '99959800'

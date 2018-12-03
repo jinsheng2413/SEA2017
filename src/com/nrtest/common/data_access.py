1819d09b33d3514ad6f28d47f918df9e4020db45
oslakesx <oslake@qq.com>
2018-12-03 11:42:18 +0800

    恢复getCaseData函数

--- src/com/nrtest/common/data_access.py
+++ src/com/nrtest/common/data_access.py
@@ -47,27 +47,34 @@ class DataAccess:
         return org_path
 
     @staticmethod
-    def getCaseData(menuNo, tabName='', groupNo=''):
+    def getCaseData(menuNo, tabName='', groupNo='', valCheck=False):
+        """
+        根据菜单编号提取测试用例数据
+        :param menuNo: 菜单编号
+        :param tabName: Tab页名
+        :param groupNo: 用例组编号
+        :param valCheck: True-对元素数据有效性校验；False-测试用例数据
+        :return: 返回用例数据
+        """
         pyoracle = PyOracle.getInstance()
         qry = [Setting.GROUP_USER, menuNo, groupNo, tabName]
-        tst_case = pyoracle.callfunc('pkg_nrtest.get_tst_case', 'str', qry)
 
-        # 字符串转list
+        # funName = 'pkg_nrtest.get_tst_case_for_valid' if valCheck else 'pkg_nrtest.get_tst_case'
+        # tst_case = pyoracle.callfunc(funName, 'str', qry)
+
+        funName = 'pkg_nrtest.get_tst_case_for_valid_cur' if valCheck else 'pkg_nrtest.get_tst_case_cur'
+        tst_case = pyoracle.callFCur(funName, qry)
+        print(tst_case)
         try:
-            rslt = eval(tst_case)
+            rslt = []
+            for row in tst_case:
+                rslt.append(Dict(eval(row[0])))
             if len(rslt) == 0:
                 print(
-                    '请确认以下配置项是否正确：\n1,配置文件（nari_test.conf）的user_group项：%s \n2,菜单编号：%s' % tuple(qry))
+                    '没有user_group：{} 与菜单编号：{} 的测试用例数据'.format(qry[0], qry[1]))
             print('当前用例数据：\n', rslt, '\n')
-
-            # dict转Dict ljf
-            for i in range(len(rslt)):
-                rslt[i] = Dict(rslt[i])
         except BaseException:
             print('获取测试用例数据失败，请检查用例用户组名称是否配置正确，用例编写是否符合要求')
-
-        print('---------')
-
         return rslt
 
     @staticmethod
@@ -123,9 +130,9 @@ class DataAccess:
 
 if __name__ == '__main__':
     # DataAccess.refresh_case()
-    # str = DataAccess.getCaseData("99911500")
+    print(DataAccess.getCaseData("99913220"))
     # print(type(str))
-    print(DataAccess.get_case_result('999111003'))
+    # print(DataAccess.get_case_result('999111003'))
     # val = Dict(eval(str[4]['ORG_NO']))
     # print(val['FLAG'], val['VALUE'])
 

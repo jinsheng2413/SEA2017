f97fd671d3f4ea397b90ea189f74b63373dd1dbb
hx <389197447@qq.com>
2018-12-06 16:11:22 +0800

    细节修改
    Signed-off-by: hx <389197447@qq.com>

--- src/com/nrtest/sea/pages/base_app/dataGatherMan/gatherQualityAnalyze/gatherSuccessRate_page.py
+++ src/com/nrtest/sea/pages/base_app/dataGatherMan/gatherQualityAnalyze/gatherSuccessRate_page.py
@@ -9,28 +9,11 @@
 """
 
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.base_app.dataGatherMan.gatherQualityAnalyze.gatherSuccessRate_locators import \
-    GatherSuccessRateLocators
-
+from com.nrtest.sea.locators.base_app.dataGatherMan.gatherQualityAnalyze.gatherSuccessRate_locators import *
 
 # 基本应该→数据采集管理→采集质量分析→采集成功率
+# 采集成功率→采集成功率
 class GatherSuccessRatePage(Page):
-    # 采集成功率→采集成功率
-    # 页面元素
-    # 查询日期开始
-    def inputDt_start_date(self, content):
-        self.exec_script(GatherSuccessRateLocators.STARTDATE_JS)
-        self.input(content, *GatherSuccessRateLocators.START_DATE)
-
-    # 查询日期结束
-    def inputDt_end_date(self, content):
-        self.exec_script(GatherSuccessRateLocators.ENDDATE_JS)
-        self.input(content, *GatherSuccessRateLocators.END_DATE)
-
-    # 查询按钮
-    def btn_search(self):
-        self.click(*GatherSuccessRateLocators.BTN_SEARCH)
-
     # 用户类型
     def inputCSel_cons_type(self, index):
         if index == 'c':
@@ -42,131 +25,266 @@ class GatherSuccessRatePage(Page):
             self.click(*locator)
             self.click(*GatherSuccessRateLocators.CONS_TYPE)
 
-    # 采集成功率→采集成功率统计
-    # 采集成功率统计
-    def btn_statistics(self):
-        self.click(*GatherSuccessRateLocators.BTN_STATISTICS)
+    # 通信方式
+    def inputSel_comm_type(self, index):
+        self.click(*GatherSuccessRateLocators.COMM_WAY)
+        locator = self.get_select_locator(
+            GatherSuccessRateLocators.COMM_WAY_VALUE, index)
+        self.click(*locator)
 
-    # 页面元素
-    # 查询日期
-    def inputDt_statistics_date(self, content):
-        self.exec_script(GatherSuccessRateLocators.STATISTICS_DATE_JS)
-        self.input(content, *GatherSuccessRateLocators.STATISTICS_DATE)
+    # 终端厂家
+    def inputSel_tmnl_factory(self, index):
+        self.click(*GatherSuccessRateLocators.TMNL_FACTORY)
+        locator = self.get_select_locator(
+            GatherSuccessRateLocators.TMNL_FACTORY_VALUE, index)
+        self.click(*locator)
 
-    # 用户类型
-    def inputCSel_statistics_cons_type(self, index):
-        if index == 'c':
-            self._find_element(*GatherSuccessRateLocators.STATISTICS_CONS_TYPE)
-        else:
-            self.click(*GatherSuccessRateLocators.STATISTICS_CONS_TYPE)
-            locator = self.get_select_locator(
-                GatherSuccessRateLocators.STATISTICS_CONS_TYPE_VALUE, index)
-            self.click(*locator)
-            self.click(*GatherSuccessRateLocators.STATISTICS_CONS_TYPE)
+    # 计量方式
+    def inputSel_measure_way(self, index):
+        self.click(*GatherSuccessRateLocators.MEASURE_WAY)
+        locator = self.get_select_locator(
+            GatherSuccessRateLocators.MEASURE_WAY_VALUE, index)
+        self.click(*locator)
 
-    # 查询按钮
-    def btn_statistics_search(self):
-        self.click(*GatherSuccessRateLocators.BTN_STATISTICS_SEARCH)
+    # 所属区域
+    def inputSel_area(self, index):
+        self.click(*GatherSuccessRateLocators.AREA)
+        locator = self.get_select_locator(
+            GatherSuccessRateLocators.AREA_VALUE, index)
+        self.click(*locator)
 
-    # 采集成功率→数据采集成功率明细
-    # 采集成功率统计
-    def btn_detail(self):
-        self.click(*GatherSuccessRateLocators.BTN_DETAIL)
+    # 芯片厂家
+    def inputSel_chip_factory(self, index):
+        self.click(*GatherSuccessRateLocators.CHIP_FACTORY)
+        locator = self.get_select_locator(
+            GatherSuccessRateLocators.CHIP_FACTORY_VALUE, index)
+        self.click(*locator)
 
-    # 页面元素
-    # 查询日期
-    def inputDt_detail_date(self, content):
-        self.exec_script(GatherSuccessRateLocators.DETAIL_DATE_JS)
-        self.input(content, *GatherSuccessRateLocators.DETAIL_DATE)
+    # 查询日期开始
+    def inputDt_start_date(self, content):
+        self.exec_script(GatherSuccessRateLocators.START_DATE_JS)
+        self.input(content, *GatherSuccessRateLocators.START_DATE)
 
-    # 用户类型
-    def inputCSel_detail_cons_type(self, index):
-        if index == 'c':
-            self._find_element(*GatherSuccessRateLocators.DETAIL_CONS_TYPE)
-        else:
-            self.click(*GatherSuccessRateLocators.DETAIL_CONS_TYPE)
-            locator = self.get_select_locator(
-                GatherSuccessRateLocators.DETAIL_CONS_TYPE_VALUE, index)
-            self.click(*locator)
-            self.click(*GatherSuccessRateLocators.DETAIL_CONS_TYPE)
+    # 查询日期结束
+    def inputDt_end_date(self, content):
+        self.exec_script(GatherSuccessRateLocators.END_DATE_JS)
+        self.input(content, *GatherSuccessRateLocators.END_DATE)
 
     # 查询按钮
-    def btn_detail_search(self):
-        self.click(*GatherSuccessRateLocators.BTN_DETAIL_SEARCH)
+    def btn_search(self):
+        self.click(*GatherSuccessRateLocators.BTN_SEARCH)
 
-    # 未抄数
-    def btn_detail_never(self):
-        self.click(*GatherSuccessRateLocators.BTN_DETAIL_NEVER)
 
-    # 采集成功率→连续抄表失败明细
-    # 连续抄表失败明细
-    def btn_continuous_false(self):
-        self.click(*GatherSuccessRateLocators.BTN_CONTINUOUS_FALSE)
-
-    # 页面元素
-    # 查询日期
-    def inputDt_false_date(self, content):
-        self.exec_script(GatherSuccessRateLocators.FALSE_DATE_JS)
-        self.input(content, *GatherSuccessRateLocators.FALSE_DATE)
-
+# 采集成功率→采集成功率统计
+class GatherSuccessRateStatPage(Page):
     # 用户类型
-    def inputCSel_false_cons_type(self, index):
+    def inputCSel_cons_type(self, index):
         if index == 'c':
-            self._find_element(*GatherSuccessRateLocators.FALSE_CONS_TYPE)
+            self._find_element(*GatherSuccessRateStatLocators.CONS_TYPE)
         else:
-            self.click(*GatherSuccessRateLocators.FALSE_CONS_TYPE)
+            self.click(*GatherSuccessRateStatLocators.CONS_TYPE)
             locator = self.get_select_locator(
-                GatherSuccessRateLocators.FALSE_CONS_TYPE_VALUE, index)
+                GatherSuccessRateStatLocators.CONS_TYPE_VALUE, index)
             self.click(*locator)
-            self.click(*GatherSuccessRateLocators.FALSE_CONS_TYPE)
+            self.click(*GatherSuccessRateStatLocators.CONS_TYPE)
+
+    # 通信方式
+    def inputSel_comm_type(self, index):
+        self.click(*GatherSuccessRateStatLocators.COMM_WAY)
+        locator = self.get_select_locator(
+            GatherSuccessRateStatLocators.COMM_WAY_VALUE, index)
+        self.click(*locator)
+
+    # 终端厂家
+    def inputSel_tmnl_factory(self, index):
+        self.click(*GatherSuccessRateStatLocators.TMNL_FACTORY)
+        locator = self.get_select_locator(
+            GatherSuccessRateStatLocators.TMNL_FACTORY_VALUE, index)
+        self.click(*locator)
+
+    # 芯片厂家
+    def inputSel_chip_factory(self, index):
+        self.click(*GatherSuccessRateStatLocators.CHIP_FACTORY)
+        locator = self.get_select_locator(
+            GatherSuccessRateStatLocators.CHIP_FACTORY_VALUE, index)
+        self.click(*locator)
+
+    # 通讯规约
+    def inputSel_tmnl_protocol(self, index):
+        self.click(*GatherSuccessRateStatLocators.TMNL_PROTOCOL)
+        locator = self.get_select_locator(
+            GatherSuccessRateStatLocators.TMNL_PROTOCOL_VALUE, index)
+        self.click(*locator)
+
+    # 查询日期
+    def inputDt_date(self, content):
+        self.exec_script(GatherSuccessRateStatLocators.DATE_JS)
+        self.input(content, *GatherSuccessRateStatLocators.DATE)
 
     # 查询按钮
-    def btn_false_search(self):
-        self.click(*GatherSuccessRateLocators.BTN_FALSE_SEARCH)
+    def btn_search(self):
+        self.click(*GatherSuccessRateStatLocators.BTN_SEARCH)
 
-    # 连续N天抄表失败明细
-    def btn_false_detail(self):
-        self.click(*GatherSuccessRateLocators.BTN_FALSE_DETAIL)
 
-    # 基本应用→数据采集管理→采集质量分析→采集成功率→按时间统计
+# 采集成功率→数据采集成功率明细
+class GatherSuccessRateDetailPage(Page):
+    # 用户类型
+    def inputCSel_cons_type(self, index):
+        if index == 'c':
+            self._find_element(*GatherSuccessRateDetailLocators.CONS_TYPE)
+        else:
+            self.click(*GatherSuccessRateDetailLocators.CONS_TYPE)
+            locator = self.get_select_locator(
+                GatherSuccessRateDetailLocators.CONS_TYPE_VALUE, index)
+            self.click(*locator)
+            self.click(*GatherSuccessRateDetailLocators.CONS_TYPE)
+
+    # 芯片厂家
+    def inputSel_chip_factory(self, index):
+        self.click(*GatherSuccessRateDetailLocators.CHIP_FACTORY)
+        locator = self.get_select_locator(
+            GatherSuccessRateDetailLocators.CHIP_FACTORY_VALUE, index)
+        self.click(*locator)
+
+    # 终端厂家
+    def inputSel_tmnl_factory(self, index):
+        self.click(*GatherSuccessRateDetailLocators.TMNL_FACTORY)
+        locator = self.get_select_locator(
+            GatherSuccessRateDetailLocators.TMNL_FACTORY_VALUE, index)
+        self.click(*locator)
+
+    # 通信方式
+    def inputSel_comm_type(self, index):
+        self.click(*GatherSuccessRateDetailLocators.COMM_WAY)
+        locator = self.get_select_locator(
+            GatherSuccessRateDetailLocators.COMM_WAY_VALUE, index)
+        self.click(*locator)
+
+    # 用户编号
+    def inputStr_cons_no(self, content):
+        self.input(content, *GatherSuccessRateDetailLocators.CONS_NO)
+
+    # 终端地址
+    def inputStr_tmnl_addr(self, content):
+        self.input(content, *GatherSuccessRateDetailLocators.TMNL_ADDR)
+
+    # 查询日期
+    def inputDt_date(self, content):
+        self.exec_script(GatherSuccessRateDetailLocators.DATE_JS)
+        self.input(content, *GatherSuccessRateDetailLocators.DATE)
+
+    # 查询按钮
+    def btn_search(self):
+        self.click(*GatherSuccessRateDetailLocators.BTN_SEARCH)
+
+
+# 采集成功率→连续抄表失败明细
+class ContinuousFalseDetailPage(Page):
+    # 用户类型
+    def inputCSel_cons_type(self, index):
+        if index == 'c':
+            self._find_element(*ContinuousFalseDetailLocators.CONS_TYPE)
+        else:
+            self.click(*ContinuousFalseDetailLocators.CONS_TYPE)
+            locator = self.get_select_locator(
+                ContinuousFalseDetailLocators.CONS_TYPE_VALUE, index)
+            self.click(*locator)
+            self.click(*ContinuousFalseDetailLocators.CONS_TYPE)
+
+    # 运行状态
+    def inputSel_run_status(self, index):
+        self.click(*ContinuousFalseDetailLocators.RUN_STATUS)
+        locator = self.get_select_locator(
+            ContinuousFalseDetailLocators.RUN_STATUS_VALUE, index)
+        self.click(*locator)
+
+    # 查询日期
+    def inputDt_date(self, content):
+        self.exec_script(ContinuousFalseDetailLocators.DATE_JS)
+        self.input(content, *ContinuousFalseDetailLocators.DATE)
+
+    # 查询按钮
+    def btn_search(self):
+        self.click(*ContinuousFalseDetailLocators.BTN_SEARCH)
+
+
+# 基本应用→数据采集管理→采集质量分析→采集成功率→按时间统计
+class GatherSuccessRateTimePage(Page):
     # 查询日期，开始
-    def inputDt_data_start_date(self, content):
-        self.exec_script(GatherSuccessRateLocators.DATE_START_DATE_JS)
-        self.input(content, *GatherSuccessRateLocators.DATE_START_DATE)
+    def inputDt_start_date(self, content):
+        self.exec_script(GatherSuccessRateTimeLocators.START_DATE_JS)
+        self.input(content, *GatherSuccessRateTimeLocators.START_DATE)
 
     # 查询日期，结束
-    def inputDt_data_end_date(self, content):
-        self.exec_script(GatherSuccessRateLocators.DATE_END_DATE_JS)
-        self.input(content, *GatherSuccessRateLocators.DATE_END_DATE)
+    def inputDt_end_date(self, content):
+        self.exec_script(GatherSuccessRateTimeLocators.END_DATE_JS)
+        self.input(content, *GatherSuccessRateTimeLocators.END_DATE)
 
     # 用户类型
-    def inputCSel_date_cons_type(self, index):
+    def inputCSel_cons_type(self, index):
         if index == 'c':
-            self._find_element(*GatherSuccessRateLocators.DATE_CONS_TYPE)
+            self._find_element(*GatherSuccessRateTimeLocators.CONS_TYPE)
         else:
-            self.click(*GatherSuccessRateLocators.DATE_CONS_TYPE)
+            self.click(*GatherSuccessRateTimeLocators.CONS_TYPE)
             locator = self.get_select_locator(
-                GatherSuccessRateLocators.DATE_CONS_TYPE_VALUE, index)
+                GatherSuccessRateTimeLocators.CONS_TYPE_VALUE, index)
             self.click(*locator)
-            self.click(*GatherSuccessRateLocators.DATE_CONS_TYPE)
+            self.click(*GatherSuccessRateTimeLocators.CONS_TYPE)
+
+    # 用户范围
+    def inputSel_user_range(self, index):
+        self.click(*GatherSuccessRateTimeLocators.USER_RANGE)
+        locator = self.get_select_locator(
+            GatherSuccessRateTimeLocators.USER_RANGE_VALUE, index)
+        self.click(*locator)
+
+    # 停电标志
+    def inputSel_power_cut_sign(self, index):
+        self.click(*GatherSuccessRateTimeLocators.POWER_CUT_SIGN)
+        locator = self.get_select_locator(
+            GatherSuccessRateTimeLocators.POWER_CUT_SIGN_VALUE, index)
+        self.click(*locator)
+
+    # 终端类型
+    def inputCSel_tmnl_type(self, index):
+        if index == 'c':
+            self._find_element(*GatherSuccessRateTimeLocators.TMNL_TYPE)
+        else:
+            self.click(*GatherSuccessRateTimeLocators.TMNL_TYPE)
+            locator = self.get_select_locator(
+                GatherSuccessRateTimeLocators.TMNL_TYPE_VALUE, index)
+            self.click(*locator)
+            self.click(*GatherSuccessRateTimeLocators.TMNL_TYPE)
+
+    # 通信方式
+    def inputCSel_comm_way(self, index):
+        if index == 'c':
+            self._find_element(*GatherSuccessRateTimeLocators.COMM_WAY)
+        else:
+            self.click(*GatherSuccessRateTimeLocators.COMM_WAY)
+            locator = self.get_select_locator(
+                GatherSuccessRateTimeLocators.COMM_WAY_VALUE, index)
+            self.click(*locator)
+            self.click(*GatherSuccessRateTimeLocators.COMM_WAY)
+
+    # 规约类型
+    def inputCSel_protocol_type(self, index):
+        if index == 'c':
+            self._find_element(*GatherSuccessRateTimeLocators.PROTOCOL_TYPE)
+        else:
+            self.click(*GatherSuccessRateTimeLocators.PROTOCOL_TYPE)
+            locator = self.get_select_locator(
+                GatherSuccessRateTimeLocators.PROTOCOL_TYPE_VALUE, index)
+            self.click(*locator)
+            self.click(*GatherSuccessRateTimeLocators.PROTOCOL_TYPE)
+
+    # 计量方式
+    def inputSel_measure_way(self, index):
+        self.click(*GatherSuccessRateTimeLocators.MEASURE_WAY)
+        locator = self.get_select_locator(
+            GatherSuccessRateTimeLocators.MEASURE_WAY_VALUE, index)
+        self.click(*locator)
 
     # 查询按钮
-    def btn_date_search(self):
-        self.click(*GatherSuccessRateLocators.BTN_DATE_SEARCH)
-
-    # 显示区
-    # 第一个单位
-    def btn_first_unit(self):
-        self.click(*GatherSuccessRateLocators.BTN_FIRST_UNIT)
-
-    # 第一个“更多”按钮
-    def btn_first_more(self):
-        self.click(*GatherSuccessRateLocators.BTN_FIRST_MORE)
-
-    # 跳出窗口关闭按钮
-    def btn_close(self):
-        self.click(*GatherSuccessRateLocators.BTN_CLOSE)
-
-    # 第一个结果的采集成功率
-    def show_first_statistics(self):
-        self.click(*GatherSuccessRateLocators.SHOW_FIRST_STATISTICS)
+    def btn_search(self):
+        self.click(*GatherSuccessRateTimeLocators.BTN_SEARCH)

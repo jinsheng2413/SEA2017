6cc67fac6af375e24443d020fed5059e3b99d878
jinsheng <543045780@qq.com>
2018-12-24 10:36:27 +0800

    基站信息维护--修改
    Signed-off-by: jinsheng <543045780@qq.com>

--- src/com/nrtest/sea/testcase/run_man/runStatusMonitor/MComMan230/test_baseStationMonitor.py
+++ src/com/nrtest/sea/testcase/run_man/runStatusMonitor/MComMan230/test_baseStationMonitor.py
@@ -44,16 +44,46 @@ class TestDemo(unittest.TestCase, BaseStationManagePage):
         ddt实现参数化（tst_case_detail数据表），通过key值，出入对应的值
         key值要与tst_case_detail表中的XPATH_NAME的值保持一致
         """
+        # 注册菜单
+        self.menu_name = para['MENU_NAME']
         sleep(2)
         # 通信地址
         self.inputStr_communicationAddr(para['COMMUNICATION_ADDR'])
         self.btn_qry()
         self.sleep_time(2)
-        # 校验
-        result = self.assert_context(*BaseStationManageLocators.TAB_ONE)
+
+    def assert_query_result(self, para):
+        """
+        查询结果校验
+        :param para:
+        """
+        self.assertTrue(self.check_query_result(para))
+
+    def assert_query_criteria(self, para):
+        """
+        查询条件校验
+        :param para:
+        """
+        result = self.check_query_criteria(para)
         self.assertTrue(result)
 
     @BeautifulReport.add_test_img()
     @data(*DataAccess.getCaseData(MComMan230.baseStationManage_para))
     def test_query(self, para):
+        """
+        对查询结果有无、数据链接跳转等校验
+        :param para: 用例数据
+        :return:
+        """
+        self.start_case(para)
         self.query(para)
+        self.assert_query_result(para)
+        self.end_case(para)
+
+    @BeautifulReport.add_test_img()
+    @data(*DataAccess.getCaseData(MComMan230.baseStationManage_para,valCheck=True))
+    def _test_checkValue(self, para):
+        self.start_case(para)
+        self.query(para)
+        self.assert_query_criteria(para)
+        self.end_case(para)

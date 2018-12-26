cddf188930c04acf90c21ed034065d1ff97a8121
luyanyan <luyanyan@qq.com>
2018-12-26 13:52:27 +0800

    范例说明

--- src/com/nrtest/sea/testcase/demo_new/test_TmnlInstallDetai_debug.py
+++ src/com/nrtest/sea/testcase/demo_new/test_TmnlInstallDetai_debug.py
@@ -30,6 +30,7 @@ class TestTmnlInstallDetai_debug(TestCase, TmnlInstallDetaiPage):
         super(TestCase, cls).__init__(cls, menuPage.driver, menuPage)
         # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
         menuPage.clickTabPage(DataGatherMan_data.tmnlInstallDetail_tabOne)
+        # 菜单页面上如果没日期型的查询条件时，请注释下面代码
         menuPage.remove_dt_readonly()
 
     @classmethod

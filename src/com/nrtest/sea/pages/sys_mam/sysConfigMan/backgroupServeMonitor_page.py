1e30915496eac054828a4cea9965b7ad87f01c47
luyanyan <luyanyan@qq.com>
2018-12-26 18:08:08 +0800

    系统管理→系统配置管理→后台服务监测

--- src/com/nrtest/sea/pages/sys_mam/sysConfigMan/backgroupServeMonitor_page.py
+++ src/com/nrtest/sea/pages/sys_mam/sysConfigMan/backgroupServeMonitor_page.py
@@ -9,42 +9,49 @@
 """
 
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.sys_mam.sysConfigMan.backgroupServeMonitor_locators import *
 
 
 # 系统管理→系统配置管理→后台服务监测
 class BackgroupServeMonitorPage(Page):
     # 查询日期
     def inputDt_date(self, content):
-        self.exec_script(BackgroupServeMonitorLocators.DATE_JS)
-        self.input(content, *BackgroupServeMonitorLocators.QRY_DATE)
+        # self.exec_script(BackgroupServeMonitorLocators.DATE_JS)
+        # self.input(content, *BackgroupServeMonitorLocators.QRY_DATE)
+        self.inputDate(content, True)
 
     # 运行状态
-    def inputSel_operation_stat(self, index):
-        self.click(*BackgroupServeMonitorLocators.QRY_OPERATION_STAT)
-        locator = self.get_select_locator(BackgroupServeMonitorLocators.QRY_OPERATION_STAT_VALUE, index)
-        self.click(*locator)
+    def inputSel_operation_stat(self, options):
+        # self.click(*BackgroupServeMonitorLocators.QRY_OPERATION_STAT)
+        # locator = self.get_select_locator(BackgroupServeMonitorLocators.QRY_OPERATION_STAT_VALUE, options)
+        # self.click(*locator)
+        self.selectDropDown(options, is_multi_tab=True, is_multi_elements=True)
+
 
     # 查询按钮
     def btn_search(self):
-        self.click(*BackgroupServeMonitorLocators.BTN_SEARCH)
+        # self.click(*BackgroupServeMonitorLocators.BTN_SEARCH)
+        self.btn_query(True)
 
 
 # 系统管理→系统配置管理→后台服务监测→后台服务监测明细
 class BackgroupServeMonitorDetailPage(Page):
     # JOB名称
-    def inputStr_job_name(self, index):
-        self.input(index, *BackgroupServeMonitorDetailLocators.QRY_JOB_NAME)
+    def inputStr_job_name(self, content):
+        # self.input(index, *BackgroupServeMonitorDetailLocators.QRY_JOB_NAME)
+        self.input(content)
 
     # 服务名称
-    def inputStr_serve_name(self, index):
-        self.input(index, *BackgroupServeMonitorDetailLocators.QRY_SERVE_NAME)
+    def inputStr_serve_name(self, content):
+        # self.input(index, *BackgroupServeMonitorDetailLocators.QRY_SERVE_NAME)
+        self.input(content)
 
     # 查询日期
     def inputDt_date(self, content):
-        self.exec_script(BackgroupServeMonitorDetailLocators.DATE_JS)
-        self.input(content, *BackgroupServeMonitorDetailLocators.QRY_DATE)
+        # self.exec_script(BackgroupServeMonitorDetailLocators.DATE_JS)
+        # self.input(content, *BackgroupServeMonitorDetailLocators.QRY_DATE)
+        self.input(content)
 
     # 查询按钮
     def btn_search(self):
-        self.click(*BackgroupServeMonitorDetailLocators.BTN_SEARCH)
+        # self.click(*BackgroupServeMonitorDetailLocators.BTN_SEARCH)
+        self.btn_query(True)
--- src/com/nrtest/sea/testcase/sys_mam/sysConfigMan/test_backgroupServeMonitor.py
+++ src/com/nrtest/sea/testcase/sys_mam/sysConfigMan/test_backgroupServeMonitor.py
@@ -26,8 +26,15 @@ class TestBackgroupServeMonitor(unittest.TestCase, BackgroupServeMonitorPage):
     @classmethod
     def setUpClass(cls):
         print('开始执行')
-        # 打开菜单（需要传入对应的菜单编号）
-        cls.driver = openMenu(SysConfigManData.BackgroundServeMonitor_para)
+        # # 打开菜单（需要传入对应的菜单编号）
+        # cls.driver = openMenu(SysConfigManData.BackgroundServeMonitor_para)
+        # 打开菜单（需要传入对应的菜单编号）ljf
+        menuPage = MenuPage.openMenu(SysConfigManData.BackgroundServeMonitor_para)
+        super(unittest.TestCase, cls).__init__(cls, menuPage.driver, menuPage)
+        # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
+        menuPage.clickTabPage(SysConfigManData.BackgroupServeMonitorDetail_tabName)
+        # 菜单页面上如果没日期型的查询条件时，请注释下面代码
+        menuPage.remove_dt_readonly()
 
     @classmethod
     def tearDownClass(cls):
@@ -47,7 +54,7 @@ class TestBackgroupServeMonitor(unittest.TestCase, BackgroupServeMonitorPage):
         :return:
         """
         # 回收左边树
-        self.recoverLeftTree()
+        # self.recoverLeftTree()
 
     def query(self, para):
         sleep(2)
@@ -58,9 +65,39 @@ class TestBackgroupServeMonitor(unittest.TestCase, BackgroupServeMonitorPage):
         # 查询按钮
         self.btn_search()
 
+    # @BeautifulReport.add_test_img()
+    # @data(
+    #     *DataAccess.getCaseData(SysConfigManData.BackgroundServeMonitor_para,
+    #                             SysConfigManData.BackgroupServeMonitor_tabName))
+    # def test_der(self, para):
+    #     self.query(para)
+    def assert_query_result(self, para):
+        """
+        查询结果校验（包括跳转）
+        :param para:
+        """
+        self.assertTrue(self.check_query_result(para))
+
+    def assert_query_criteria(self, para):
+        """
+        查询条件校验
+        :param para:
+        """
+        result = self.check_query_criteria(para)
+        self.assertTrue(result)
+
     @BeautifulReport.add_test_img()
-    @data(
-        *DataAccess.getCaseData(SysConfigManData.BackgroundServeMonitor_para,
-                                SysConfigManData.BackgroupServeMonitor_tabName))
-    def test_der(self, para):
+    @data(*DataAccess.getCaseData(SysConfigManData.BackgroundServeMonitor_para, SysConfigManData.BackgroupServeMonitorDetail_tabName))
+    def test_query(self, para):
+        self.start_case(para)
         self.query(para)
+        self.assert_query_result(para)
+        self.end_case(para)
+
+    @BeautifulReport.add_test_img()
+    @data(*DataAccess.getCaseData(SysConfigManData.BackgroundServeMonitor_para, SysConfigManData.BackgroupServeMonitorDetail_tabName, valCheck=True))
+    def _test_checkValue(self, para):
+        self.start_case(para)
+        self.query(para)
+        self.assert_query_criteria(para)
+        self.end_case(para)

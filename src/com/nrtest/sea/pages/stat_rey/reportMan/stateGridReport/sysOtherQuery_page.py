3741606b5a9abcba84fab729569e8863a596e4ba
js <1234567@qq.com>
2018-12-27 18:39:45 +0800

    国网报表--其他运行指标--修改

--- src/com/nrtest/sea/pages/stat_rey/reportMan/stateGridReport/sysOtherQuery_page.py
+++ src/com/nrtest/sea/pages/stat_rey/reportMan/stateGridReport/sysOtherQuery_page.py
@@ -7,24 +7,29 @@
 @desc:
 """
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.stat_rey.reportMan.stateGridReport.sysOtherQuery_locators import SysOtherQueryLocators
 
 
 # 统计查询--》报表管理--》国网报表--》系统其他运行指标
 
 class SysOtherQueryPage(Page):
 
+    # 统计分类
+    def inputChk_stat_type(self, option):
+        self.clickRadioBox(option)
+
     # 查询日期
 
     def inputStr_dateS(self, value):
-        self.input(value, *SysOtherQueryLocators.QRY_DATE)
+        self.inputDate(value)  # , *SysOtherQueryLocators.QRY_DATE)
 
     # 统计口径
-    def inputSel_statWay(self, index):
-        self.click(*SysOtherQueryLocators.QRY_STAT_WAY)
-        locator = self.get_select_locator(SysOtherQueryLocators.QRY_STAT_WAY_VALUE, index)
-        self.click(*locator)
+    def inputSel_statWay(self, option):
+        # self.click(*SysOtherQueryLocators.QRY_STAT_WAY)
+        # locator = self.get_select_locator(SysOtherQueryLocators.QRY_STAT_WAY_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(option)
 
     # 查询
     def btn_qry(self):
-        self.click(*SysOtherQueryLocators.BTN_QRY)
+        # self.click(*SysOtherQueryLocators.BTN_QRY)
+        self.btn_query()

8e5950a7eabc51438eacc0a1e1432bb7591d1030
luyanyan <luyanyan@qq.com>
2018-12-27 09:25:57 +0800

    用户操作监测

--- src/com/nrtest/sea/pages/sys_mam/sysConfigMan/userOperationMan_page.py
+++ src/com/nrtest/sea/pages/sys_mam/sysConfigMan/userOperationMan_page.py
@@ -9,26 +9,29 @@
 """
 
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.sys_mam.sysConfigMan.userOperationMan_locators import UserOperationMonitorLocators
 
 
 # 系统管理→系统配置管理→用户操作监测
 class UserOperationMonitorPage(Page):
     # 日期
     def inputDt_date(self, content):
-        self.exec_script(UserOperationMonitorLocators.DATE_JS)
-        self.input(content, *UserOperationMonitorLocators.QRY_DATE)
+        # self.exec_script(UserOperationMonitorLocators.DATE_JS)
+        # self.input(content, *UserOperationMonitorLocators.QRY_DATE)
+        self.inputDate(content)
 
     # 操作模块
-    def inputSel_operation_module(self, index):
-        self.click(*UserOperationMonitorLocators.QRY_OPERATION_MODULE)
-        locator = self.get_select_locator(UserOperationMonitorLocators.QRY_OPERATION_MODULE_VALUE, index)
-        self.click(*locator)
+    def inputSel_operation_module(self, option):
+        # self.click(*UserOperationMonitorLocators.QRY_OPERATION_MODULE)
+        # locator = self.get_select_locator(UserOperationMonitorLocators.QRY_OPERATION_MODULE_VALUE, option)
+        # self.click(*locator)
+        self.selectDropDown(option)
 
     # 操作人员
     def inputStr_operator(self, content):
-        self.input(content, *UserOperationMonitorLocators.QRY_OPERATOR)
+        # self.input(content, *UserOperationMonitorLocators.QRY_OPERATOR)
+        self.input(content)
 
     # 查询按钮
     def btn_search(self):
-        self.click(*UserOperationMonitorLocators.BTN_SEARCH)
+        # self.click(*UserOperationMonitorLocators.BTN_SEARCH)
+        self.btn_query()
--- src/com/nrtest/sea/testcase/sys_mam/sysConfigMan/test_userOperationMan.py
+++ src/com/nrtest/sea/testcase/sys_mam/sysConfigMan/test_userOperationMan.py
@@ -9,15 +9,13 @@
 """
 
 import unittest
-from time import sleep
 
 from ddt import ddt, data
 
 from com.nrtest.common.BeautifulReport import BeautifulReport
 from com.nrtest.common.data_access import DataAccess
 from com.nrtest.sea.data.sys_mam.sysConfigMan.sysConfigMan_data import SysConfigManData
-from com.nrtest.sea.pages.sys_mam.sysConfigMan.userOperationMan_page import UserOperationMonitorPage, \
-    UserOperationMonitorLocators
+from com.nrtest.sea.pages.sys_mam.sysConfigMan.userOperationMan_page import UserOperationMonitorPage
 from com.nrtest.sea.task.commonMath import *
 
 
@@ -27,8 +25,15 @@ class TestUserOperationMonitor(unittest.TestCase, UserOperationMonitorPage):
     @classmethod
     def setUpClass(cls):
         print('开始执行')
-        # 打开菜单（需要传入对应的菜单编号）
-        cls.driver = openMenu(SysConfigManData.UserOperationMonitor_para)
+        # # 打开菜单（需要传入对应的菜单编号）
+        # cls.driver = openMenu(SysConfigManData.UserOperationMonitor_para)
+        # 打开菜单（需要传入对应的菜单编号）ljf
+        menuPage = MenuPage.openMenu(SysConfigManData.UserOperationMonitor_para)
+        super(unittest.TestCase, cls).__init__(cls, menuPage.driver, menuPage)
+        # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
+        # menuPage.clickTabPage(SysConfigManData.SysAbnormalParaSet_tabName)
+        # 菜单页面上如果没日期型的查询条件时，请注释下面代码
+        menuPage.remove_dt_readonly()
 
     @classmethod
     def tearDownClass(cls):
@@ -51,9 +56,12 @@ class TestUserOperationMonitor(unittest.TestCase, UserOperationMonitorPage):
         self.recoverLeftTree()
 
     def query(self, para):
+        # self.displayTreeMenu()
+        # sleep(2)
         self.displayTreeMenu()
+
         # 打开左边树并选择
-        openLeftTree(para['TREE_NODE'])  # 'TREE_ORG_NO'])
+        self.openLeftTree(para['TREE_NODE'])
         # 日期
         self.inputDt_date(para['DATE'])
         # 操作模块
@@ -62,13 +70,44 @@ class TestUserOperationMonitor(unittest.TestCase, UserOperationMonitorPage):
         self.inputStr_operator(para['OPERATOR'])
         # 查询按钮
         self.btn_search()
-        # 校验
-        sleep(2)
-        result = self.assert_context(*UserOperationMonitorLocators.CHECK_FIRST)
+
+    #     # 校验
+    #     sleep(2)
+    #     result = self.assert_context(*UserOperationMonitorLocators.CHECK_FIRST)
+    #     self.assertTrue(result)
+    #
+    # @BeautifulReport.add_test_img()
+    # @data(
+    #     *DataAccess.getCaseData(SysConfigManData.UserOperationMonitor_para))
+    # def test_der(self, para):
+    #     self.query(para)
+    def assert_query_result(self, para):
+        """
+        查询结果校验（包括跳转）
+        :param para:
+        """
+        self.assertTrue(self.check_query_result(para))
+
+    def assert_query_criteria(self, para):
+        """
+        查询条件校验
+        :param para:
+        """
+        result = self.check_query_criteria(para)
         self.assertTrue(result)
 
     @BeautifulReport.add_test_img()
-    @data(
-        *DataAccess.getCaseData(SysConfigManData.UserOperationMonitor_para))
-    def test_der(self, para):
+    @data(*DataAccess.getCaseData(SysConfigManData.UserOperationMonitor_para))
+    def test_query(self, para):
+        self.start_case(para)
         self.query(para)
+        self.assert_query_result(para)
+        self.end_case(para)
+
+    @BeautifulReport.add_test_img()
+    @data(*DataAccess.getCaseData(SysConfigManData.UserOperationMonitor_para, valCheck=True))
+    def _test_checkValue(self, para):
+        self.start_case(para)
+        self.query(para)
+        self.assert_query_criteria(para)
+        self.end_case(para)

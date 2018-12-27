01a1ab9e0f9b01ee1e3875b63df0ea876256d09c
js <1234567@qq.com>
2018-12-27 16:40:27 +0800

    国网报表--采集系统运行指标--修改

--- src/com/nrtest/sea/pages/stat_rey/reportMan/stateGridReport/sysRunContrastQuery_page.py
+++ src/com/nrtest/sea/pages/stat_rey/reportMan/stateGridReport/sysRunContrastQuery_page.py
@@ -10,21 +10,23 @@
 """
 
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.stat_rey.reportMan.stateGridReport.sysRunContrastQuery_locators import \
-    SysRunContrastQueryLocators
 
 
 # 统计查询--》报表管理--》国网报表--》采集系统运行指标
 class SysRunContrastQueryPage(Page):
+
     def inputStr_date(self, value):
-        self.input(value, *SysRunContrastQueryLocators.QRY_DATE)
+        self.inputDate(value)  # , *SysRunContrastQueryLocators.QRY_DATE)
 
     # 统计口径
 
-    def inputSel_statWay(self, index):
-        self.click(*SysRunContrastQueryLocators.QRY_STAT_WAY)
-        locator = self.get_select_locator(SysRunContrastQueryLocators.QRY_STAT_WAY_VALUE, index)
-        self.click(*locator)
+    def inputSel_statWay(self, option):
+        # self.click(*SysRunContrastQueryLocators.QRY_STAT_WAY)
+        # locator = self.get_select_locator(SysRunContrastQueryLocators.QRY_STAT_WAY_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(option)
+
     # 查询
     def btn_qry(self):
-        self.click(*SysRunContrastQueryLocators.BTN_QRY)
+        # self.click(*SysRunContrastQueryLocators.BTN_QRY)
+        self.btn_query()

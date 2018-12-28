785aa0ad29f9f4d36c8c6e6a514ad28cefab2487
guochunbiao <guochunbiao00416@163.com>
2018-12-28 14:15:04 +0800

    "线损统计查询"

--- src/com/nrtest/sea/pages/adv_app/lineLossAnalysis/lineLossStatisticsAnalysis/lineLossStatisticsQuery_page.py
+++ src/com/nrtest/sea/pages/adv_app/lineLossAnalysis/lineLossStatisticsAnalysis/lineLossStatisticsQuery_page.py
@@ -9,35 +9,38 @@
 """
 
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.adv_app.lineLossAnalysis.lineLossStatisticsAnalysis.lineLossStatisticsQuery_locators import \
-    LineLossStatisticsQueryLocators
 
 
 # 高级应用→线损分析→线损统计分析→线损统计查询
 class LineLossStatisticsQueryPage(Page):
     # 线损分类
     def inputSel_line_loss_type(self, index):
-        self.click(*LineLossStatisticsQueryLocators.QRY_LINE_LOSS_TYPE)
-        locator = self.get_select_locator(
-            LineLossStatisticsQueryLocators.QRY_LINE_LOSS_TYPE_VALUE, index)
-        self.click(*locator)
+        # self.click(*LineLossStatisticsQueryLocators.QRY_LINE_LOSS_TYPE)
+        # locator = self.get_select_locator(
+        #     LineLossStatisticsQueryLocators.QRY_LINE_LOSS_TYPE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index)
 
     # 线损率
     def inputSel_line_loss_rate(self, index):
-        self.click(*LineLossStatisticsQueryLocators.QRY_LINE_LOSS_RATE)
-        locator = self.get_select_locator(
-            LineLossStatisticsQueryLocators.QRY_LINE_LOSS_RATE_VALUE, index)
-        self.click(*locator)
+        # self.click(*LineLossStatisticsQueryLocators.QRY_LINE_LOSS_RATE)
+        # locator = self.get_select_locator(
+        #     LineLossStatisticsQueryLocators.QRY_LINE_LOSS_RATE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index)
 
     def inputStr_line_loss_rate(self, content):
-        self.input(
-            content, *LineLossStatisticsQueryLocators.QRY_LINE_LOSS_RATE_INPUT)
+        # self.input(
+        #     content, *LineLossStatisticsQueryLocators.QRY_LINE_LOSS_RATE_INPUT)
+        self.input(content)
 
     # 日期
     def inputDt_date(self, content):
-        self.exec_script(LineLossStatisticsQueryLocators.DATE_JS)
-        self.input(content, *LineLossStatisticsQueryLocators.QRY_DATE)
+        # self.exec_script(LineLossStatisticsQueryLocators.DATE_JS)
+        # self.input(content, *LineLossStatisticsQueryLocators.QRY_DATE)
+        self.inputDate(content)
 
     # 查询按钮
     def btn_search(self):
-        self.click(*LineLossStatisticsQueryLocators.BTN_SEARCH)
+        # self.click(*LineLossStatisticsQueryLocators.BTN_SEARCH)
+        self.curr_click(btn_name='查   询')

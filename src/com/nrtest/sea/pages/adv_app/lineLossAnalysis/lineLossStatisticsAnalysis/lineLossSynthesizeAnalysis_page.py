ffc2a55cc5f055f7e8cd0e9931e61cdf1fa7f59e
guochunbiao <guochunbiao00416@163.com>
2019-01-02 16:46:30 +0800

    # 高级应用→线损分析→线损统计分析→线损综合分析 # 高级应用→线损分析→线损统计分析→分压线损分析 新模式

--- src/com/nrtest/sea/pages/adv_app/lineLossAnalysis/lineLossStatisticsAnalysis/lineLossSynthesizeAnalysis_page.py
+++ src/com/nrtest/sea/pages/adv_app/lineLossAnalysis/lineLossStatisticsAnalysis/lineLossSynthesizeAnalysis_page.py
@@ -8,24 +8,29 @@
 @desc:
 """
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.adv_app.lineLossAnalysis.lineLossStatisticsAnalysis.lineLossSynthesizeAnalysis_locators import \
-    LineLossSynthesizeAnalysisLocators
 
 
 # 高级应用→线损分析→线损统计分析→线损综合分析
 class LineLossSynthesizeAnalysisPage(Page):
     # 线损类别
     def inputSel_line_loss_type(self, index):
-        self.click(LineLossSynthesizeAnalysisLocators.QRY_LINE_LOSS_TYPE)
-        locator = self.get_select_locator(
-            LineLossSynthesizeAnalysisLocators.QRY_LINE_LOSS_TYPE_VALUE, index)
-        self.click(locator)
+        # self.click(*LineLossSynthesizeAnalysisLocators.QRY_LINE_LOSS_TYPE)
+        # locator = locator = self.get_select_locator(
+        #     LineLossSynthesizeAnalysisLocators.QRY_LINE_LOSS_TYPE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index)
 
     # 查询日期
     def inputDt_date(self, content):
-        self.exec_script(LineLossSynthesizeAnalysisLocators.DATE_JS)
-        self.input(content, *LineLossSynthesizeAnalysisLocators.QRY_DATE)
+        # self.exec_script(LineLossSynthesizeAnalysisLocators.DATE_JS)
+        ###self.input(content, *LineLossSynthesizeAnalysisLocators.QRY_DATE)
+        self.inputDate(content)
+
+    # 按日期统计类型
+    def inputDTTAB_statDateType(self, name):
+        self.clickDt_Tab(name)
 
     # 查询按钮
     def btn_search(self):
-        self.click(LineLossSynthesizeAnalysisLocators.BTN_SEARCH)
+        # self.click(*LineLossSynthesizeAnalysisLocators.BTN_SEARCH)
+        self.btn_query()
--- src/com/nrtest/sea/pages/adv_app/lineLossAnalysis/lineLossStatisticsAnalysis/partialPressureLineLossAnalysis_page.py
+++ src/com/nrtest/sea/pages/adv_app/lineLossAnalysis/lineLossStatisticsAnalysis/partialPressureLineLossAnalysis_page.py
@@ -9,28 +9,35 @@
 """
 
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.adv_app.lineLossAnalysis.lineLossStatisticsAnalysis.partialPressureLineLossAnalysis_locators import \
-    PartialPressureLineLossAnalysisLocators
 
 
 # 高级应用→线损分析→线损统计分析→分压线损分析
 class PartialPressureLineLossAnalysisPage(Page):
     # 电压等级
     def inputSel_voltage_level(self, index):
-        if index == 'c':
-            self._find_element(PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL)
-        else:
-            self.click(PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL)
-            locator = self.get_select_locator(
-                PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL_VALUE, index)
-            self.click(locator)
-            self.click(PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL)
+        # if index == 'c':
+        #     self._find_element(
+        #         *PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL)
+        # else:
+        #     self.click(
+        #         *PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL)
+        #     locator = self.get_select_locator(
+        #         PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL_VALUE, index)
+        #     self.click(*locator)
+        #     self.click(
+        #         *PartialPressureLineLossAnalysisLocators.QRY_VOLTAGE_LEVEL)
+        self.selectCheckBox(index)
 
+    # 按日期统计类型
+    def inputDTTAB_statDateType(self, name):
+        self.clickDt_Tab(name)
     # 查询日期
     def inputDt_date(self, content):
-        self.exec_script(PartialPressureLineLossAnalysisLocators.DATE_JS)
-        self.input(content, *PartialPressureLineLossAnalysisLocators.QRY_DATE)
+        # self.exec_script(PartialPressureLineLossAnalysisLocators.DATE_JS)
+        # self.input(content, *PartialPressureLineLossAnalysisLocators.QRY_DATE)
+        self.inputDate(content)
 
     # 查询按钮
     def btn_search(self):
-        self.click(PartialPressureLineLossAnalysisLocators.BTN_SEARCH)
+        # self.click(*PartialPressureLineLossAnalysisLocators.BTN_SEARCH)
+        self.btn_query()

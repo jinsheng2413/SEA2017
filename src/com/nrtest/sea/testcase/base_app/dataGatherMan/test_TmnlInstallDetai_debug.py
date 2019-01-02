cf7d6ef95fd277630ff7b2bce75ec432edfa6b4e
js <1234567@qq.com>
2019-01-02 16:44:24 +0800

    基本应用--终端管理--远程调试--修改
    Signed-off-by: js <1234567@qq.com>

--- src/com/nrtest/sea/testcase/base_app/dataGatherMan/test_TmnlInstallDetai_debug.py
+++ src/com/nrtest/sea/testcase/base_app/dataGatherMan/test_TmnlInstallDetai_debug.py
@@ -7,8 +7,8 @@
 @time: 2018/9/10 0010 9:21
 @desc:
 """
-import unittest
-from time import sleep
+
+from unittest import TestCase
 
 from ddt import ddt, data
 
@@ -19,27 +19,20 @@ from com.nrtest.sea.pages.base_app.dataGatherMan.tmnlInstallDetai_page import Tm
 from com.nrtest.sea.task.commonMath import *
 
 
+# 基本应用--终端管理--远程调试（第二个tab页）
 @ddt
-class TestTmnlInstallDetai_debug(unittest.TestCase, TmnlInstallDetaiPage):
+class TestTmnlInstallDetai_debug(TestCase, TmnlInstallDetaiPage):
 
     @classmethod
     def setUpClass(cls):
         print("开始执行")
         # 打开菜单（需要传入对应的菜单编号）
-        # 打开菜单（需要传入对应的菜单编号）
-        cls.driver = openMenu(DataGatherMan_data.tmnlInstallDetail_para)
-
-        clickTabPage(DataGatherMan_data.tmnlInstallDetail_tabOne)
-        sleep(2)
-        # cls.exec_script(cls, TmnlInstallDetaiLocators.START_DATE_JS_COUNT)
-        # cls.exec_script(cls, TmnlInstallDetaiLocators.END_DATE_JS_COUNT)
-        cls.remove_dt_readonly(cls)
-
-        # 下拉选择无需移除readonly属性
-        # cls.exec_script(cls, TmnlInstallDetaiLocators.LCT_JS_COUNT)
-        # cls.exec_script(cls, TmnlInstallDetaiLocators.METER_TYPE_JS_COUNT)
-        # cls.exec_script(cls, TmnlInstallDetaiLocators.TMNL_FACTORY_JS_COUNT)
-        # cls.exec_script(cls, TmnlInstallDetaiLocators.TMNL_TYPE_JS_COUNT)
+        menuPage = MenuPage.openMenu(DataGatherMan_data.tmnlInstallDetail_para)
+        super(TestCase, cls).__init__(cls, menuPage.driver, menuPage)
+        # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
+        menuPage.clickTabPage(DataGatherMan_data.tmnlInstallDetail_tabOne)
+        # 菜单页面上如果没日期型的查询条件时，请注释下面代码
+        menuPage.remove_dt_readonly()
 
     @classmethod
     def tearDownClass(cls):
@@ -69,33 +62,42 @@ class TestTmnlInstallDetai_debug(unittest.TestCase, TmnlInstallDetaiPage):
         ddt实现参数化（tst_case_detail数据表），通过key值，出入对应的值
         key值要与tst_case_detail表中的XPATH_NAME的值保持一致
         """
-        # 注册菜单
-        self.menu_name = para['MENU_NAME']
-
         # 打开左边树并选择
-        openLeftTree(para['TREE_NODE'])  # 'ORG_NO'])
+        self.openLeftTree(para['TREE_NODE'])
+
         # 开始时间
         self.inputStr_startTime_count(para['START_TIME'])
+
         # 结束时间
         self.inputStr_endTime_Count(para['END_TIME'])
+
         # 运行状态
         self.inputSel_runState_count(para['RUN_STATE'])
+
         # 流程标识
         self.inputSel_processID_count(para['PROCESS_ID'])
+
         # 申请单号
         self.inputStr_applyNo_count(para['APPLY_NO'])
+
         # 用户编号
         self.inputStr_userNo_count(para['USER_NO'])
+
         # 终端地址
         self.inputStr_tmnlAddr_count(para["TMNL_ADDR"])
+
         # 终端厂家
         self.inputSel_tmnlFactory_count(para['TMNL_FACTORY'])
+
         # 装接类型
         self.inputSel_moutingType_count(para['MOUNTING_TYPE'])
+
         # 终端类型
         self.inputSel_tmnlType_count(para['TMNL_TYPE'])
+
         # 通信规约
         self.inputSel_LCT_count(para['LCT'])
+
         # 表类型
         self.inputSel_surfaceType_count(para['SURFACE_TYPE'])
 

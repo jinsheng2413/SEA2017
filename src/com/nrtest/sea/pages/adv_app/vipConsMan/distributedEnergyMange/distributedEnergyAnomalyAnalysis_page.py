7a8efd59da1b271631c37ecc472b021893cd1996
jinsheng <543045780@qq.com>
2019-01-02 11:00:25 +0800

    细节修改

--- src/com/nrtest/sea/pages/adv_app/vipConsMan/distributedEnergyMange/distributedEnergyAnomalyAnalysis_page.py
+++ src/com/nrtest/sea/pages/adv_app/vipConsMan/distributedEnergyMange/distributedEnergyAnomalyAnalysis_page.py
@@ -9,63 +9,78 @@
 """
 
 from com.nrtest.common.base_page import Page
-from com.nrtest.sea.locators.adv_app.vipConsMan.distributedEnergyMange.distributedEnergyAnomalyAnalysis_locators import *
 
 
 # 高级应用→重点用户监测→分布式电源管理→分布式电源异常分析
 class DistributedEnergyAnomalyAnalysisPage(Page):
+    # 查询类型
+    def inputChk_qry_type(self, index):
+        self.clickRadioBox(index)
+
     # 日期
     def inputDt_date(self, content):
-        self.exec_script(DistributedEnergyAnomalyAnalysisLocators.DATE_JS)
-        self.input(content, *DistributedEnergyAnomalyAnalysisLocators.QRY_DATE)
+        # self.exec_script(DistributedEnergyAnomalyAnalysisLocators.DATE_JS)
+        # self.input(content, *DistributedEnergyAnomalyAnalysisLocators.QRY_DATE)
+        self.inputDate(content)
 
     # 发电类型
     def inputSel_elec_type(self, index):
-        self.click(*DistributedEnergyAnomalyAnalysisLocators.QRY_ELEC_TYPE)
-        locator = self.get_select_locator(
-            DistributedEnergyAnomalyAnalysisLocators.QRY_ELEC_TYPE_VALUE, index)
-        self.click(*locator)
+        # self.click(*DistributedEnergyAnomalyAnalysisLocators.QRY_ELEC_TYPE)
+        # locator = self.get_select_locator(
+        #     DistributedEnergyAnomalyAnalysisLocators.QRY_ELEC_TYPE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index)
 
     # 发电量消纳方式
     def inputSel_abso_type(self, index):
-        self.click(*DistributedEnergyAnomalyAnalysisLocators.QRY_ABSO_MODE)
-        locator = self.get_select_locator(
-            DistributedEnergyAnomalyAnalysisLocators.QRY_ABSO_MODE_VALUE, index)
-        self.click(*locator)
+        # self.click(*DistributedEnergyAnomalyAnalysisLocators.QRY_ABSO_MODE)
+        # locator = self.get_select_locator(
+        #     DistributedEnergyAnomalyAnalysisLocators.QRY_ABSO_MODE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index)
 
     # 查询按钮
     def btn_search(self):
-        self.click(*DistributedEnergyAnomalyAnalysisLocators.BTN_SEARCH)
-
+        # self.click(*DistributedEnergyAnomalyAnalysisLocators.BTN_SEARCH)
+        self.btn_query()
 
 # 高级应用→重点用户监测→分布式电源管理→分布式电源异常分析→分布式电源异常情况明细
 class DistributedEnergyAnomalyDetailPage(Page):
+    # 查询类型
+    def inputChk_qry_type(self, index):
+        self.clickRadioBox(index, is_multi_tab=True, is_multi_elements=True)
+
     # 日期
     def inputDt_date(self, content):
-        self.exec_script(DistributedEnergyAnomalyDetailLocators.DATE_JS)
-        self.input(content, *DistributedEnergyAnomalyDetailLocators.QRY_DATE)
+        # self.exec_script(DistributedEnergyAnomalyDetailLocators.DATE_JS)
+        # self.input(content, *DistributedEnergyAnomalyDetailLocators.QRY_DATE)
+        self.inputDate(content)
 
     # 异常类型
     def inputSel_anomaly_type(self, index):
-        self.click(*DistributedEnergyAnomalyDetailLocators.QRY_ANOMALY_TYPE)
-        locator = self.get_select_locator(
-            DistributedEnergyAnomalyDetailLocators.QRY_ANOMALY_TYPE_VALUE, index)
-        self.click(*locator)
+        # self.click(*DistributedEnergyAnomalyDetailLocators.QRY_ANOMALY_TYPE)
+        # locator = self.get_select_locator(
+        #     DistributedEnergyAnomalyDetailLocators.QRY_ANOMALY_TYPE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index)
 
     # 发电类型
     def inputSel_elec_type(self, index):
-        self.click(*DistributedEnergyAnomalyDetailLocators.QRY_ELEC_TYPE)
-        locator = self.get_select_locator(
-            DistributedEnergyAnomalyDetailLocators.QRY_ELEC_TYPE_VALUE, index)
-        self.click(*locator)
+        # self.click(*DistributedEnergyAnomalyDetailLocators.QRY_ELEC_TYPE)
+        # locator = self.get_select_locator(
+        #     DistributedEnergyAnomalyDetailLocators.QRY_ELEC_TYPE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index, is_multi_elements=True, is_multi_tab=True)
 
     # 发电量消纳方式
     def inputSel_abso_type(self, index):
-        self.click(*DistributedEnergyAnomalyDetailLocators.QRY_ABSO_MODE)
-        locator = self.get_select_locator(
-            DistributedEnergyAnomalyDetailLocators.QRY_ABSO_MODE_VALUE, index)
-        self.click(*locator)
+        # self.click(*DistributedEnergyAnomalyDetailLocators.QRY_ABSO_MODE)
+        # locator = self.get_select_locator(
+        #     DistributedEnergyAnomalyDetailLocators.QRY_ABSO_MODE_VALUE, index)
+        # self.click(*locator)
+        self.selectDropDown(index, is_multi_tab=True, is_multi_elements=True)
 
     # 查询按钮
     def btn_search(self):
-        self.click(*DistributedEnergyAnomalyDetailLocators.BTN_SEARCH)
+        # self.click(*DistributedEnergyAnomalyDetailLocators.BTN_SEARCH)
+        self.btn_query(True)
--- src/com/nrtest/sea/testcase/adv_app/vipConsMan/distributedEnergyMange/test_distributedEnergyLoadMonitor.py
+++ src/com/nrtest/sea/testcase/adv_app/vipConsMan/distributedEnergyMange/test_distributedEnergyLoadMonitor.py
@@ -8,7 +8,7 @@
 @desc:
 """
 
-import unittest
+from unittest import TestCase
 
 from ddt import ddt, data
 
@@ -23,12 +23,17 @@ from com.nrtest.sea.task.commonMath import *
 
 # 高级应用→重点用户监测→分布式电源管理→分布式电源负荷监测
 @ddt
-class TestDistributedEnergyLoadMonitor(unittest.TestCase, DistributedEnergyLoadMonitorPage):
+class TestDistributedEnergyLoadMonitor(TestCase, DistributedEnergyLoadMonitorPage):
     @classmethod
     def setUpClass(cls):
         print('开始执行')
-        # 打开菜单（需要传入对应的菜单编号）
-        cls.driver = openMenu(DistributedEnergyMange_data.DistributedEnergyLoadMonitor_para)
+        # 打开菜单（需要传入对应的菜单编号）ljf
+        menuPage = MenuPage.openMenu(DistributedEnergyMange_data.DistributedEnergyLoadMonitor_para)
+        super(TestCase, cls).__init__(cls, menuPage.driver, menuPage)
+        # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
+        menuPage.clickTabPage(DistributedEnergyMange_data.DistributedEnergyLoadMonitor_tabName)
+        # 菜单页面上如果没日期型的查询条件时，请注释下面代码
+        menuPage.remove_dt_readonly()
 
     @classmethod
     def tearDownClass(cls):
@@ -52,18 +57,50 @@ class TestDistributedEnergyLoadMonitor(unittest.TestCase, DistributedEnergyLoadM
 
     def query(self, para):
         # 打开左边树并选择
-        openLeftTree(para['TREE_NODE'])  # 'TREE_ORG_NO'])
+        self.openLeftTree(para['TREE_NODE'])
+        # 统计方式
+        self.inputChk_stat_way(para['STAT_WAY'])
         # 日期
         self.inputDt_date(para['DATE'])
         # 发电类型
         self.inputSel_elec_type(para['ELEC_TYPE'])
         # 发电量消纳方式
         self.inputSel_abso_type(para['ABSO_TYPE'])
+        # 负荷类型
+        self.inputChk_load_type(para['LOAD_TYPE'])
         # 查询按钮
         self.btn_search()
 
+    def assert_query_result(self, para):
+        """
+        查询结果校验（包括跳转）
+        :param para:
+        """
+        self.assertTrue(self.check_query_result(para))
+
+    def assert_query_criteria(self, para):
+        """
+        查询条件校验
+        :param para:
+        """
+        result = self.check_query_criteria(para)
+        self.assertTrue(result)
+
     @BeautifulReport.add_test_img()
     @data(
-        *DataAccess.getCaseData(DistributedEnergyMange_data.DistributedEnergyLoadMonitor_para, tabName='分布式电源负荷监测统计'))
-    def test_der(self, para):
+        *DataAccess.getCaseData(DistributedEnergyMange_data.DistributedEnergyLoadMonitor_para,
+                                DistributedEnergyMange_data.DistributedEnergyLoadMonitor_tabName))
+    def test_query(self, para):
+        self.start_case(para)
         self.query(para)
+        self.assert_query_result(para)
+        self.end_case(para)
+
+    @BeautifulReport.add_test_img()
+    @data(*DataAccess.getCaseData(DistributedEnergyMange_data.DistributedEnergyLoadMonitor_para,
+                                  DistributedEnergyMange_data.DistributedEnergyLoadMonitor_tabName, valCheck=True))
+    def _test_checkValue(self, para):
+        self.start_case(para)
+        self.query(para)
+        self.assert_query_criteria(para)
+        self.end_case(para)

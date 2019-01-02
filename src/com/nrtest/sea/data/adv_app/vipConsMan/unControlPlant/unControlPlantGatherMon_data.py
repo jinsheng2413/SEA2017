78e1acc11ad1a88029ff5927cda172500ee3cddc
jinsheng <543045780@qq.com>
2019-01-02 16:28:57 +0800

    细节修改

--- src/com/nrtest/sea/data/adv_app/vipConsMan/unControlPlant/unControlPlantGatherMon_data.py
+++ src/com/nrtest/sea/data/adv_app/vipConsMan/unControlPlant/unControlPlantGatherMon_data.py
@@ -16,3 +16,5 @@ class UnControlPlant:
     para_unControlPlantGatherMon_tab_detail = '非统调电厂采集监测明细'
     # 高级应用→重点用户监测→非统调电厂管理→非统调电厂接入统计
     UnControlPlantStat_para = '99926210'
+    UnControlPlantStat_tabName = '非统调电厂接入统计'
+    UnControlPlantStat_tabName_Detail = '非统调电厂接入明细'
--- src/com/nrtest/sea/testcase/adv_app/vipConsMan/unControlPlant/test_unControlPlantDetail.py
+++ src/com/nrtest/sea/testcase/adv_app/vipConsMan/unControlPlant/test_unControlPlantDetail.py
@@ -8,7 +8,7 @@
 @desc:
 """
 
-import unittest
+from unittest import TestCase
 
 from ddt import ddt, data
 
@@ -22,12 +22,17 @@ from com.nrtest.sea.task.commonMath import *
 
 # 高级应用→重点用户监测→非统调电厂管理→非统调电厂接入统计→非统调电厂接入明细
 @ddt
-class TestUnControlPlantDetail(unittest.TestCase, UnControlPlantDetailPage):
+class TestUnControlPlantDetail(TestCase, UnControlPlantDetailPage):
     @classmethod
     def setUpClass(cls):
         print('开始执行')
-        # 打开菜单（需要传入对应的菜单编号）
-        cls.driver = openMenu(UnControlPlant.UnControlPlantStat_para)
+        # 打开菜单（需要传入对应的菜单编号）ljf
+        menuPage = MenuPage.openMenu(UnControlPlant.UnControlPlantStat_para)
+        super(TestCase, cls).__init__(cls, menuPage.driver, menuPage)
+        # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
+        menuPage.clickTabPage(UnControlPlant.UnControlPlantStat_tabName_Detail)
+        # 菜单页面上如果没日期型的查询条件时，请注释下面代码
+        menuPage.remove_dt_readonly()
 
     @classmethod
     def tearDownClass(cls):
@@ -50,9 +55,8 @@ class TestUnControlPlantDetail(unittest.TestCase, UnControlPlantDetailPage):
         self.recoverLeftTree()
 
     def query(self, para):
-        clickTabPage('非统调电厂接入明细')
         # 打开左边树并选择
-        openLeftTree(para['TREE_NODE'])  # 'TREE_ORG_NO'])
+        self.openLeftTree(para['TREE_NODE'])
         # 发电方式
         self.inputSel_elec_way(para['ELEC_WAY'])
         # 采集方式
@@ -68,8 +72,40 @@ class TestUnControlPlantDetail(unittest.TestCase, UnControlPlantDetailPage):
         # 查询按钮
         self.btn_search()
 
+    def assert_query_result(self, para):
+        """
+        查询结果校验
+        :param para:
+        """
+        self.assertTrue(self.check_query_result(para))
+
+    def assert_query_criteria(self, para):
+        """
+        查询条件校验
+        :param para:
+        """
+        result = self.check_query_criteria(para)
+        self.assertTrue(result)
+
     @BeautifulReport.add_test_img()
-    @data(
-        *DataAccess.getCaseData(UnControlPlant.UnControlPlantStat_para, tabName='非统调电厂接入明细'))
-    def test_der(self, para):
+    @data(*DataAccess.getCaseData(UnControlPlant.UnControlPlantStat_para,
+                                  UnControlPlant.UnControlPlantStat_tabName_Detail))
+    def test_query(self, para):
+        """
+        对查询结果有无、数据链接跳转等校验
+        :param para: 用例数据
+        :return:
+        """
+        self.start_case(para)
         self.query(para)
+        self.assert_query_result(para)
+        self.end_case(para)
+
+    @BeautifulReport.add_test_img()
+    @data(*DataAccess.getCaseData(UnControlPlant.UnControlPlantStat_para,
+                                  UnControlPlant.UnControlPlantStat_tabName_Detail, valCheck=True))
+    def _test_checkValue(self, para):
+        self.start_case(para)
+        self.query(para)
+        self.assert_query_criteria(para)
+        self.end_case(para)

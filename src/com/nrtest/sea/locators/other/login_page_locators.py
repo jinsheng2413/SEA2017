ce08975558f0d2e76b6875fcc6a0ba11b8ae5468
oslakesx <oslake@qq.com>
2019-01-07 15:20:36 +0800

    LOGIN优化处理

--- src/com/nrtest/sea/locators/other/login_page_locators.py
+++ src/com/nrtest/sea/locators/other/login_page_locators.py
@@ -12,17 +12,44 @@ from selenium.webdriver.common.by import By
 
 class LoginPageLocators:
     # 【输入框】
-    QRY_USERNAME = (By.XPATH, '//*[@id="staffNo_input"]')
-    QRY_PASSWWORD = (By.XPATH, '//*[@id="password_input"]')
-    INPUT_IDENTIFYING = (By.XPATH, '//*[@id="checkcode_input"]')
+    # -->用户名
+    # 国网标设
+    QRY_USERNAME = (By.XPATH, '//input[@id="staffNo_input"]')
+    # 电量PBS5000
+    # QRY_USERNAME = (By.XPATH, '//input[@id="nameLogin"]')
+
+    # -->密码
+    # 国网标设
+    QRY_PASSWWORD = (By.XPATH, '//input[@id="password_input"]')
+    # 电量PBS5000
+    # QRY_PASSWWORD = (By.XPATH, '//input[@id="passwordLogin"]')
+
+    # -->验证码
+    INPUT_IDENTIFYING = (By.XPATH, '//input[@id="checkcode_input"]')
+
 
     # 【操作区】
-    BTN_LOGIN = (By.XPATH, '//*[@id="inputform"]/table/tbody/tr[5]/td[2]/img')
-    BTN_CONFIRM = (By.XPATH, "//button[contains(.,'确定')]")
-    BTN_ARROW = (
-        By.XPATH, '//*[@id="index.loginExceptionWin"]/div[1]/div/div/div/div[1]')
-    # 验证码
+    # -->登录按钮
+    # 国网标设
+    BTN_LOGIN = (By.XPATH, '//img[@src="./images/loginbutton.png"]')
+    # 电量PBS5000
+    # BTN_LOGIN = (By.XPATH, '//button[@id = "buttonLogin"]')
+
+    # 验证码刷新按钮
     BTN_IDENTIFYING_CODE = (By.XPATH, '//*[@id="imageMask"]')
-    #
+
+    # 登录异常弹窗确认
+    BTN_CONFIRM = (By.XPATH, "//button[contains(.,'确定')]")
+
+    # 账号异常信息弹窗确认
+    BTN_ARROW = (By.XPATH, '//*[@id="index.loginExceptionWin"]/div[1]/div/div/div/div[1]')
+
+    # 【校验区】
+    # -->系统登录成功标志值
+    # 国网标设
+    LOGIN_SUCCESS = (By.XPATH, '//span[text()="工作台"]')
+    # 电量PBS5000
+    # LOGIN_SUCCESS = (By.XPATH, '//i[@onclick="modifypassword()"]')
+
     BTN_INFORMATAIN_PUS = (By.XPATH, "//span[contains(text(),'重要信息推出')]")
 

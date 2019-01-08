88f221d65f789051ef27e56b830c5e2902f7469c
guochunbiao <guochunbiao00416@163.com>
2019-01-08 16:14:20 +0800

    rf

---
+++ src/com/nrtest/sea/data/adv_app/costControlManage/localCostControl/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 郭春彪
+@license: (C) Copyright 2018, Nari.
+@file: __init__.py.py
+@time: 2019/1/8 0008 8:23
+@desc:
+"""
---
+++ src/com/nrtest/sea/data/adv_app/costControlManage/localCostControl/localCostControl_para.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 郭春彪
+@license: (C) Copyright 2018, Nari.
+@file: localCostControl_para.py
+@time: 2019/1/8 0008 8:23
+@desc:
+"""
+
+
+class LocalCostContral_data:
+    # 高级应用--》费控管理--》本地费控--》电价参数下发
+    elePricePara_para = '99922110'
+    # 高级应用--》费控管理--》本地费控--》低压用户余额查看
+    lowUserBalanceCheck_para = '99922130'
+    lowUserBalanceCheck_check_tab = '余额查看'
+    lowUserBalanceCheck_count_tab = '余额统计'

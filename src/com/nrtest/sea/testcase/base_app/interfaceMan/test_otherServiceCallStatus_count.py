4e23d1931faa57af7aedbc7c2b71fd63bbb4d2d5
guochunbiao <guochunbiao00416@163.com>
2019-01-10 11:22:08 +0800

    # 基本应用--接口管理--其他业务接口--服务调用情况 新模式

--- src/com/nrtest/sea/testcase/base_app/interfaceMan/test_otherServiceCallStatus_count.py
+++ src/com/nrtest/sea/testcase/base_app/interfaceMan/test_otherServiceCallStatus_count.py
@@ -3,8 +3,8 @@
 """
 @author: jinsheng
 @license: (C) Copyright 2018, Nari.
-@file: test_mServiceCallStatus2.py
-@time: 2018-10-31 9:14
+@file: test_otherServiceCallStatus_count.py
+@time: 2018-10-15 15:00
 @desc:
 """
 
@@ -14,23 +14,26 @@ from ddt import ddt, data
 
 from com.nrtest.common.BeautifulReport import BeautifulReport
 from com.nrtest.common.data_access import DataAccess
-from com.nrtest.sea.data.base_app.interfaceMan.mServiceCallStatus2_data import InterfaceMan_data
-from com.nrtest.sea.pages.base_app.interfaceMan.mServiceCallStatus2_page import MServiceCallStatus2Page
+from com.nrtest.sea.data.base_app.interfaceMan.mServiceCallStatus_data import InterfaceMan_data
+from com.nrtest.sea.pages.base_app.interfaceMan.mServiceCallStatus_page import MServiceCallStatusPage
 from com.nrtest.sea.pages.other.menu_page import MenuPage
 
 
+# 基本应用--接口管理--其他业务接口--服务调用情况
 @ddt
-class TestMServiceCallStatus2(TestCase, MServiceCallStatus2Page):
+class TestOtherServiceCallStatus(TestCase, MServiceCallStatusPage):
+
     @classmethod
     def setUpClass(cls):
 
         # 打开菜单（需要传入对应的菜单编号）
-        menuPage = MenuPage.openMenu(InterfaceMan_data.mServiceCallStatus2_para)
+        menuPage = MenuPage.openMenu(InterfaceMan_data.para_MServiceCallStatus)
         super(TestCase, cls).__init__(cls, menuPage.driver, menuPage)
         # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
         # menuPage.clickTabPage(InterfaceMan_data.tmnlInstallDetail_tabOne)
         # 菜单页面上如果没日期型的查询条件时，请注释下面代码
         menuPage.remove_dt_readonly()
+
 
     @classmethod
     def tearDownClass(cls):
@@ -51,20 +54,23 @@ class TestMServiceCallStatus2(TestCase, MServiceCallStatus2Page):
         """
 
         # 回收左边树
-        self.recoverLeftTree()
+        # self.recoverLeftTree()
 
     def query(self, para):
         # 业务系统
         self.inputSel_business_system(para['BUSINESS_SYSTEM'])
+
         # #服务名称
         self.inputSel_business_name(para['BUSINESS_NAME'])
+
         # 开始时间
         self.inputDt_start_date(para['START_DATE'])
+
         # 结束时间
         self.inputDt_end_date(para['END_DATE'])
+
         # 查询
         self.btn_qry()
-        self.sleep_time(2)
 
     def assert_query_result(self, para):
         """
@@ -82,7 +88,8 @@ class TestMServiceCallStatus2(TestCase, MServiceCallStatus2Page):
         self.assertTrue(result)
 
     @BeautifulReport.add_test_img()
-    @data(*DataAccess.getCaseData(InterfaceMan_data.mServiceCallStatus2_para))
+    @data(*DataAccess.getCaseData(InterfaceMan_data.para_MServiceCallStatus,
+                                  InterfaceMan_data.para_MServiceCallStatus_count))
     def test_query(self, para):
         self.start_case(para, __file__)
         self.query(para)
@@ -90,7 +97,8 @@ class TestMServiceCallStatus2(TestCase, MServiceCallStatus2Page):
         self.end_case()
 
     @BeautifulReport.add_test_img()
-    @data(*DataAccess.getCaseData(InterfaceMan_data.mServiceCallStatus2_para, valCheck=True))
+    @data(*DataAccess.getCaseData(InterfaceMan_data.para_MServiceCallStatus,
+                                  InterfaceMan_data.para_MServiceCallStatus_count, valCheck=True))
     def _test_checkValue(self, para):
         self.start_case(para, __file__)
         self.query(para)

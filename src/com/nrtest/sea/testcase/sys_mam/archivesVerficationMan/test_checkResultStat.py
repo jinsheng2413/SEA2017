04782bdf34d83464c05916eb44641171caaaa4b4
js <1234567@qq.com>
2019-01-14 14:50:54 +0800

    Revert "完善格式"
    This reverts commit 2b30832

--- src/com/nrtest/sea/testcase/sys_mam/archivesVerficationMan/test_checkResultStat.py
+++ src/com/nrtest/sea/testcase/sys_mam/archivesVerficationMan/test_checkResultStat.py
@@ -96,10 +96,6 @@ class TestCheckResultStat(TestCase, CheckResultStatPage):
     @BeautifulReport.add_test_img()
     @data(*DataAccess.getCaseData(ArchivesVerficationMan_data.checkResultStat_para))
     def test_query(self, para):
-        """系统管理-->档案核查管理-->核查结果统计查询
-        对查询结果有无、数据链接跳转等校验
-        :param para: 用例数据
-        """
         self.start_case(para, __file__)
         self.query(para)
         self.assert_query_result(para)

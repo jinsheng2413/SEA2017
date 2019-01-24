0ea779531c23abadf470a5ea480bebfe2a98a5af
oslakesx <oslake@qq.com>
2019-01-25 01:27:09 +0800

    数据来源规范处理:Dict优化

--- src/com/nrtest/common/dictionary.py
+++ src/com/nrtest/common/dictionary.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-@author: 郭春彪
+@author: 李建方
 @license: (C) Copyright 2018, Nari.
 @file: dictionary.py
 @time: 2018/9/12 0012 8:56
@@ -11,4 +11,12 @@
 
 class Dict(dict):
     def __missing__(self, key):
+        print('该KEY值“{}”不存在！！'.format(key))
         return ''
+
+
+if __name__ == '__main__':
+    a = {'a': 1, 'b': 2}
+    b = Dict(a)
+    print(a['a'])
+    print(b['c'])

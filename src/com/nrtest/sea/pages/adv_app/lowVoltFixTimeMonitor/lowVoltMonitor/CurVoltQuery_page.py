9a65a1416d7e50b87ec0a4d3938132b5fb9fc509
chenyuefeng <chenyuefeng@qq.com>
2019-02-13 15:32:58 +0800

    低电压监测

---
+++ src/com/nrtest/sea/pages/adv_app/lowVoltFixTimeMonitor/lowVoltMonitor/CurVoltQuery_page.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: 陈越峰
+@license: (C) Copyright 2018, Nari.
+@file: fix_time_volt_cur_query_page.py
+@time: 2019-02-13 09:08:36
+@desc:
+"""
+
+from com.nrtest.common.base_page import Page
+
+
+# 高级应用→低压固定时间点电压电流监测→低电压监测→电压电流查询
+class CurVoltQueryPage(Page):
+    # 接线方式
+    def inputSel_wiring_type(self, option):
+        self.selectDropDown(option)
+
+    # 日期
+    def inputDt_query_date(self, value):
+        self.inputDate(value)
+
+    # 用户号
+    def inputStr_cons_no(self, value):
+        self.input(value)
+
+    # 电表资产号
+    def inputStr_met_asset_no(self, value):
+        self.input(value)
+
+    # 数据类型
+    def inputChk_data_type(self, option):
+        self.clickRadioBox(option)
+
+    # 查询
+    def btn_qry(self):
+        self.btn_query()

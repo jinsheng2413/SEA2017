76faf55ab5c289fc7c126ae0fec3316ed57df864
oslakesx <oslake@qq.com>
2019-02-18 16:19:46 +0800

    抄表段数据查询，数据展示Tab页多查询按钮处理

--- src/com/nrtest/sea/pages/stat_rey/synthQuery/sectDataQuery_page.py
+++ src/com/nrtest/sea/pages/stat_rey/synthQuery/sectDataQuery_page.py
@@ -15,11 +15,10 @@ from com.nrtest.common.base_page import Page
 class SectDataQueryPage(Page):
     # 抄表段编号
     def inputStr_mr_sect_no(self, content):
-        self.input(content)  # , *SectDataQueryLocators.MR_SECT_NO)
+        self.input(content)
 
     # 查询按钮
     def btn_search(self):
-        # self.click(SectDataQueryLocators.BTN_SEARCH)
         self.btn_query()
 
     # 数据展示
@@ -33,5 +32,4 @@ class SectDataQueryPage(Page):
 
     # 查询按钮
     def btn_tab_search(self):
-        # self.click(SectDataQueryLocators.BTN_TAB_SEARCH)
-        self.btn_query(True)
+        self.btn_query(True, idx=2)

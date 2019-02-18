b14f7a440d1038a9308ebe5bb8a2e405fe4f52f3
js <543045780@qq.com>
2019-02-18 14:42:40 +0800

    问题在线交流--修改

--- src/com/nrtest/sea/testcase/adv_app/newsanswer/test_news.py
+++ src/com/nrtest/sea/testcase/adv_app/newsanswer/test_news.py
@@ -54,7 +54,7 @@ class Test_News(TestCase, News_Page):
 
     def query(self, para):
         # 打开左边树并选择
-        self.openLeftTree(para['TREE_NODE'])
+        self.openLeftTree(para['TREE_NODE'],is_closed=True)
 
         # 问题标题
         self.inputStr_question_title(para['QUESTION_TITLE'])

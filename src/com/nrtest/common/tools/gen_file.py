be446b7d64cc0687fbc058f3fb08ca6afb7700c3
guochunbiao <guochunbiao00416@163.com>
2019-02-19 17:25:42 +0800

    # 高级应用→费控管理→远程费控→新专变用户远程费控执行

--- src/com/nrtest/common/tools/gen_file.py
+++ src/com/nrtest/common/tools/gen_file.py
@@ -13,26 +13,26 @@ import time
 from com.nrtest.common.data_access import DataAccess
 
 # 作者
-author = '李建方'
+author = '郭春彪'
 
 # 文件名，不同单词之间用下划线隔开
-file_name = 'terminalDataQuery'
+file_name = 'meter_State_Arr'
 
 # 存放菜单编号的数据文件类名
 # 路径
-data_file = r'com/nrtest/sea/data/stat_rey/synthQuery/synthQuery_data'
+data_file = r'D:\pythonworkspace\SEA2017\src\com\nrtest\sea\data\run_man\meterMan\meterClockMan_data'
 
 # 菜单编号
-menu_no = '99913210'
+menu_no = '99936200'
 
 # Tab页名【中文】，没Tab页时，填空串：''
-tab_name = '连续抄表失败明细'
+tab_name = ''
 
 # Tab页名【英文】 ，不填时，名称格式与存放菜单编号的变量名类同
-en_tab_name = 'DataDis'
+en_tab_name = ''
 
 # 生成文件存放路径
-filelistlog = r"D:\PycharmProjects\SEA2017\logs\filelistlog.log"
+filelistlog = r"D:\pythonworkspace\SEA2017\logs\filelistlog.log"
 
 # 当前执行想要生成的文件：01-生成Page文件；02-生成test文件
 page_type = '02'

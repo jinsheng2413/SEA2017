992670b7f1f4f4cee76f23e79eb4bbabaf2e93db
guochunbiao <guochunbiao00416@163.com>
2019-02-19 09:52:24 +0800

    # 运行管理→电能表管理→电能表状态维护(蒙东)

--- src/com/nrtest/sea/testcase/run_man/meterMan/test_meterStateArr.py
+++ src/com/nrtest/sea/testcase/run_man/meterMan/test_meterStateArr.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """
-@author: 吴竹筠
+@author: 郭春彪
 @license: (C) Copyright 2018, Nari.
-@file: test_meterClockMan.py
-@time: 2018/11/2 0002 14:12
+@file: test_meter_state_arr.py
+@time: 2019-02-19 08:56:34
 @desc:
 """
+
 from unittest import TestCase
 
 from ddt import ddt, data
@@ -15,26 +16,26 @@ from com.nrtest.common.BeautifulReport import BeautifulReport
 from com.nrtest.common.data_access import DataAccess
 from com.nrtest.sea.data.run_man.meterMan.meterClockMan_data import MeterClockMan_data
 from com.nrtest.sea.pages.other.menu_page import MenuPage
-from com.nrtest.sea.pages.run_man.meterMan.meterClockMan_pages import MeterClockManPage
+from com.nrtest.sea.pages.run_man.meterMan.meterStateArr_page import MeterStateArrPage
 
 
-# 运行管理→电能表管理→电能表状态查询
+# 运行管理→电能表管理→电能表状态维护(蒙东)
 @ddt
-class TestMeterClockMan(TestCase, MeterClockManPage):
+class test_MeterStateArr(TestCase, MeterStateArrPage):
 
     @classmethod
     def setUpClass(cls):
         # 打开菜单（需要传入对应的菜单编号）
-        menuPage = MenuPage.openMenu(MeterClockMan_data.meterClockMan_para)
+        menuPage = MenuPage.openMenu(MeterClockMan_data.meterStateArr_para)
         super(TestCase, cls).__init__(cls, menuPage.driver, menuPage)
         # 菜单页面没多个Tab页时，请注释clickTabPage所在行代码
-        # menuPage.clickTabPage(SysConfigManData.SysAbnormalParaSet_tabName)
+        # menuPage.clickTabPage()
         # 菜单页面上如果没日期型的查询条件时，请注释下面代码
         menuPage.remove_dt_readonly()
 
     @classmethod
     def tearDownClass(cls):
-        print('执行结束')
+        print("执行结束")
         # 关闭菜单页面
         cls.closePages(cls)
 
@@ -46,7 +47,7 @@ class TestMeterClockMan(TestCase, MeterClockManPage):
 
     def tearDown(self):
         """
-        测试结束后的操作，这里基本上都是关闭浏览器
+        每个测试用例测试结束后的操作，在这里做相关清理工作
         :return:
         """
 
@@ -60,30 +61,20 @@ class TestMeterClockMan(TestCase, MeterClockManPage):
         ddt实现参数化（tst_case_detail数据表），通过key值，出入对应的值
         key值要与tst_case_detail表中的XPATH_NAME的值保持一致
         """
-
-        # 打开左边树并选择
+        # 供电单位
         self.openLeftTree(para['TREE_NODE'])
 
-        # 电表事件类型
-        self.inputSel_event_type(para['EVENT_TYPE'])
+        # 终端状态
+        self.inputSel_tmnl_status(para['TMNL_STATUS'])
 
-        # 终端厂家
-        self.inputSel_tmnl_factory(para['TMNL_FACTORY'])
-
-        # 电表厂家
-        self.inputSel_meter_factory(para['METER_FACTORY'])
+        # 终端类型
+        self.inputSel_tmnl_type(para['TMNL_TYPE'])
 
         # 终端地址
-        self.inputStr_tmnl_addr(para['TMNL_ADDR'])
+        self.inputStr_terminal_addr(para['TERMINAL_ADDR'])
 
-        # 用户编号
-        self.inputStr_cons_no(para['CONS_NO'])
-
-        # 电表资产号
-        self.inputStr_meter_asset_no(para['METER_ASSET_NO'])
-
-        # 日期<=
-        self.inputDt_date_range(para['DATE_RANGE'])
+        # 包含下级供电单位
+        self.inputChk_contain_org(para['CONTAIN_ORG'])
 
         # 查询
         self.btn_qry()
@@ -104,11 +95,9 @@ class TestMeterClockMan(TestCase, MeterClockManPage):
         self.assertTrue(result)
 
     @BeautifulReport.add_test_img()
-    @data(*DataAccess.getCaseData(MeterClockMan_data.meterClockMan_para))
+    @data(*DataAccess.getCaseData(MeterClockMan_data.meterStateArr_para))
     def test_query(self, para):
-        """运行管理→电能表管理→电能表状态查询
-
-        :param para:
+        """运行管理→电能表管理→电能表状态维护(蒙东)
         """
         self.start_case(para, __file__)
         self.query(para)
@@ -116,7 +105,7 @@ class TestMeterClockMan(TestCase, MeterClockManPage):
         self.end_case()
 
     @BeautifulReport.add_test_img()
-    @data(*DataAccess.getCaseData(MeterClockMan_data.meterClockMan_para, valCheck=True))
+    @data(*DataAccess.getCaseData(MeterClockMan_data.meterStateArr_para, valCheck=True))
     def _test_checkValue(self, para):
         self.start_case(para, __file__)
         self.query(para)

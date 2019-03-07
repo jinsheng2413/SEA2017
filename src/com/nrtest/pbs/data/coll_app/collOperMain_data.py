f7ece8e8f13e9dab8b6c3619915b5c3767d846e2
shaoqian <shaoqian@163.com>
2019-03-07 11:33:25 +0800

    添加PBS5000采集监视和采集详情成功率的程序

---
+++ src/com/nrtest/pbs/data/coll_app/collOperMain_data.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 邵茜
+@license: (C) Copyright 2018, Nari.
+@file: collOperMain_data.py
+@time: 2019/3/6 0028 14:31
+@desc:
+"""
+
+
+class CollOperMain_data:
+    # 采集运维--采集监视
+    collMonitor_para = '0000201'
+    # 采集运维--采集详情
+    collDetail_para = '0000203'
+    collSuccRate_tab = '采集成功率'
+    collDetail_tab = '采集详情'

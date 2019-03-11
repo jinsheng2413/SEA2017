1dac16ca0ff80b5b40f8d78e44ab712c62c15043
shaoqian <shaoqian@163.com>
2019-03-11 15:59:56 +0800

    pbs5000-数据分析--对端分析程序编写

---
+++ src/com/nrtest/pbs/data/data_app/dataAnalyse_data.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 邵茜
+@license: (C) Copyright 2019, Nari.
+@file: collOperMain_data.py
+@time: 2019/3/11  14:31
+@desc:
+"""
+
+
+class DataAnalyse_data:
+    # 数据分析--对端分析
+    oppAnalyse_para = '0000401'
+    # 数据分析--停电时间统计
+    powerOffTime_para = '0000403'

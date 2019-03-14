50fa6db75f2cc5d28af2661c512561394d26b638
jinsheng <543045780@qq.com>
2019-03-14 14:20:11 +0800

    业务变更→重计算

---
+++ src/com/nrtest/pbs/page/business_change/againCount_page.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: againCount_page.py
+@time: 2019-03-14 13:59
+@desc:
+"""
+from com.nrtest.pbs.locators.business_change.againCount_locators import AgainCountLocators
+from com.nrtest.pbs.tree.tree_page import TreePBSPage
+
+
+# 业务变更→重计算
+class AgainCountPage(TreePBSPage):
+    # 开始时间
+    def inputDt_start_date(self, value):
+        self.inputDate(value)
+
+    # 结束时间
+    def inputDt_end_date(self, value):
+        self.inputDate(value)
+
+    # Tab页名称
+    def inputChk_tab_name(self, value):
+        self.click_button(value, is_multi_eles=True)
+
+    # 提交按钮
+    def btn_commit(self):
+        self.click(AgainCountLocators.BTN_COMMIT)

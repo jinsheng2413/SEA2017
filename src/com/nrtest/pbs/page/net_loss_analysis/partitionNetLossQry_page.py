e6e892efaab83cdb40d7ea5c09db2ec286f6b910
jinsheng <543045780@qq.com>
2019-03-15 15:01:15 +0800

    网损分析

---
+++ src/com/nrtest/pbs/page/net_loss_analysis/partitionNetLossQry_page.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: partitionNetLossQry_page.py
+@time: 2019-03-15 14:22
+@desc:
+"""
+
+from com.nrtest.pbs.locators.net_loss_analysis.partitionNetLossQry_locators import PartitionNetLossQryLocators
+from com.nrtest.pbs.tree.tree_page import TreePBSPage
+
+
+# 网损分析→分区网损查询:网损查询
+class PartitionNetLossQryPage(TreePBSPage):
+    # 区域
+    def inputSel_area(self, value):
+        self.selectDropDown(value)
+
+    # 时间方案
+    def inputChk_date_type(self, value):
+        self.click_button(value)
+
+    # 开始时间
+    def inputDt_start_date(self, value):
+        self.inputDate(value)
+
+    # 结束时间
+    def inputDt_end_date(self, value):
+        self.inputDate(value)
+
+    # TAB页名称
+    def inputChk_tab_name(self, value):
+        self.click_button(value)
+
+    # 查询按钮
+    def btn_qry(self):
+        self.click(PartitionNetLossQryLocators.BTN_QRY)
---
+++ src/com/nrtest/pbs/testcase/net_loss_analysis/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: 韩笑
+@license: (C) Copyright 2018, Nari.
+@file: __init__.py.py
+@time: 2019-03-15 14:24
+@desc:
+"""

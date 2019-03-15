27436cfc43afeb51dcbee59991ccc27524a5ae7f
shaoqian <shaoqian@163.com>
2019-03-15 14:44:29 +0800

    pbs5000 档案管理--采集点挂接程序编写

---
+++ src/com/nrtest/pbs/data/archives_man/ArchivesMan_data.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 邵茜
+@license: (C) Copyright 2019, Nari.
+@file: ArchivesMan_data.py
+@time: 2019/3/15 14:10
+@desc:
+"""
+
+
+class ArchivesMan_data:
+    # 档案管理--采集点挂接
+    collHitch_para = '0000104'
---
+++ src/com/nrtest/pbs/testcase/archives_man/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 邵茜
+@license: (C) Copyright 2019, Nari.
+@file: __init__.py.py
+@time: 2019/3/15 14:17
+@desc:
+"""

e8f6df34e56b152d15d5b5be29e07175fdb9c427
guochunbiao <guochunbiao00416@163.com>
2019-03-15 10:26:30 +0800

    #智能研判-->结算点日异常

--- src/com/nrtest/common/tools/gen_file_guo.py
+++ src/com/nrtest/common/tools/gen_file_guo.py
@@ -58,7 +58,7 @@ def testHead(autor=author,name="puiu",menNo=''):
 
     str_new = str.format(autor,name,time.strftime("%Y-%m-%d %H:%M", time.localtime()),name.capitalize())
     return str_new
-def PageHead(autor=author,name="puiu",menNo=''):
+def PageHead(autor=author,name=PageName,menNo=''):
     str = '# -*- coding: utf-8 -*-\n\n'+'"""\n'+ "@author:{}\n" \
            "@license: (C) Copyright 2019, Nari.\n" \
            "@file: {}.py\n" \
@@ -91,13 +91,13 @@ def getPath(menuNo):
 
 def  getPage(menuNO,tab='01',path ='',name=''):
   dic = {
-        '01': 'def inputStr_{}(self,value):\n\t  self.input(value)',
-        '03': 'def inputSel_{}(self,option):\n\t  self.selectDropDown(option)',
-        '04': 'def inputChk_{}(self,option):\n\t   self.selectCheckBox(option)',
-        '06': 'def inputDt_{}(self,value):\n\t   self.inputDate(value)',
-        '07': 'def inputChk_{}(self,value):\n\t   self.selectCheckBox(option)',
-        '09': 'def inputBt_{}(self,value):\n\t   self.click_button(value)',
-        '10': 'def inputXp_{}(self,value):\n\t   self.click_xpath(value)'
+        '01': 'def inputStr_{}(self,value):\n\t\t  self.input(value)',
+        '03': 'def inputSel_{}(self,option):\n\t\t  self.selectDropDown(option)',
+        '04': 'def inputChk_{}(self,option):\n\t\t   self.selectCheckBox(option)',
+        '06': 'def inputDt_{}(self,value):\n\t\t   self.inputDate(value)',
+        '07': 'def inputChk_{}(self,value):\n\t\t   self.selectCheckBox(option)',
+        '09': 'def inputBt_{}(self,value):\n\t\t   self.click_button(value)',
+        '10': 'def inputXp_{}(self,value):\n\t\t   self.click_xpath(value)'
     }
   data = get_data(menuNO,tab=tab)
   lis = []
@@ -108,75 +108,75 @@ def  getPage(menuNO,tab='01',path ='',name=''):
   for a,b,c,d in data:
 
       str  = b.lower()
-      lis.append('\t#'+d+'\n\t'+dic[c].format(str)+'\n\n\n')
+      lis.append('\t\t#'+d+'\n\t\t'+dic[c].format(str)+'\n\n\n')
   ph = path+'/'+name+'.py'
   with open(ph,'w',encoding='utf-8') as file:
       file.writelines(lis)
 def getTesth(data='',tabName=''):
-    str = '  @classmethod\n'\
-    '  def setUpClass(cls):\n'\
-    '\t# 打开菜单（需要传入对应的菜单编号）\n'\
-    '\tmenuPage = MenuPage.openMenu({})\n'\
-    '\tsuper(TestCase, cls).__init__(cls, menuPage.driver, menuPage)\n'\
-    '\tmenuPage.intoPBSIframe()\n'\
-    '\t# 菜单页面没多个Tab页时，请注释clickTabPage所在行代码\n'\
-    '\tmenuPage.clickTabPage({})\n'\
-    '\t# 菜单页面上如果没日期型的查询条件时，请注释下面代码\n\n'\
-    '  @classmethod\n'\
-    '  def tearDownClass(cls):\n'\
-    '\tprint("执行结束")\n'\
-    '\tcls.iframe_back(cls, num=1)\n'\
-    '\t# 关闭菜单页面\n'\
-    '\tcls.main_page(cls)\n\n'\
-    '  def setUp(self):\n'\
-    '\t"""\n'\
-    '\t测试固件的setUp()的代码，主要是测试的前提准备工作\n'\
-    '\t:return:\n'\
-    '\t"""\n\n'\
-    '  def tearDown(self):\n'\
-    '\t"""\n'\
+    str = '    @classmethod\n'\
+    '    def setUpClass(cls):\n'\
+    '\t\t# 打开菜单（需要传入对应的菜单编号）\n'\
+    '\t\tmenuPage = MenuPage.openMenu({})\n'\
+    '\t\tsuper(TestCase, cls).__init__(cls, menuPage.driver, menuPage)\n'\
+    '\t\tmenuPage.intoPBSIframe()\n'\
+    '\t\t# 菜单页面没多个Tab页时，请注释clickTabPage所在行代码\n'\
+    '\t\tmenuPage.clickTabPage({})\n'\
+    '\t\t# 菜单页面上如果没日期型的查询条件时，请注释下面代码\n\n'\
+    '    @classmethod\n'\
+    '    def tearDownClass(cls):\n'\
+    '\t\tprint("执行结束")\n'\
+    '\t\tcls.iframe_back(cls, num=1)\n'\
+    '\t\t# 关闭菜单页面\n'\
+    '\t\tcls.main_page(cls)\n\n'\
+    '    def setUp(self):\n'\
+    '\t\t"""\n'\
+    '\t\t测试固件的setUp()的代码，主要是测试的前提准备工作\n'\
+    '\t\t:return:\n'\
+    '\t\t"""\n\n'\
+    '    def tearDown(self):\n'\
+    '\t\t"""\n'\
     '\t每个测试用例测试结束后的操作，在这里做相关清理工作\n'\
-    '\t:return:\n'\
-    '\t"""\n'\
-    '\t# 回收左边树\n'\
-    '\tself.closeLeftTree()\n\n'\
-    '  def query(self, para):\n'\
-    '\t"""\n'\
-    '\t:param para: Dict类型的字典，不是dict\n'\
-    '\tddt实现参数化（tst_case_detail数据表），通过key值，出入对应的值\n'\
-    '\tkey值要与tst_case_detail表中的XPATH_NAME的值保持一致\n'\
-    '\t"""\n'
+    '\t\t:return:\n'\
+    '\t\t"""\n'\
+    '\t\t# 回收左边树\n'\
+    '\t\tself.closeLeftTree()\n\n'\
+    '    def query(self, para):\n'\
+    '\t\t"""\n'\
+    '\t\t:param para: Dict类型的字典，不是dict\n'\
+    '\t\tddt实现参数化（tst_case_detail数据表），通过key值，出入对应的值\n'\
+    '\t\tkey值要与tst_case_detail表中的XPATH_NAME的值保持一致\n'\
+    '\t\t"""\n'
     str_new = str.format(data,tabName)
     return str_new
 def testTail(para,menuPath):
-    str ='  def assert_query_result(self, para):\n'\
-    '\t"""\n'\
-    '\t查询结果校验（包括跳转）\n'\
-    '\t:param para:\n'\
-    '\t"""\n'\
-    '\t# self.assertTrue(AssertResult(self).check_query_result(para))\n'\
-    '  def assert_query_criteria(self, para):\n'\
-    '\t"""\n'\
-    '\t查询条件校验\n'\
-    '\t:param para:\n'\
-    '\t"""\n'\
-    '\t# self.assertTrue(self.check_query_criteria(para))\n\n'\
-    '  @BeautifulReport.add_test_img()\n'\
-    '  @data(*DataAccess.getCaseData({}))\n'\
-    '  def test_query(self, para):\n'\
-    '\t"""{}\n'\
-    '\t"""\n'\
-    '\tself.start_case(para, __file__)\n'\
-    '\tself.query(para)\n'\
-    '\tself.assert_query_result(para)\n'\
-    '\tself.end_case()\n\n'\
-    '  @BeautifulReport.add_test_img()\n'\
-    '  @data(*DataAccess.getCaseData({}, valCheck=True))\n'\
-    '  def _test_checkValue(self, para):\n'\
-    '\tself.start_case(para, __file__)\n'\
-    '\tself.query(para)\n'\
-    '\tself.assert_query_criteria(para)\n'\
-    '\tself.end_case()\n'
+    str ='    def assert_query_result(self, para):\n'\
+    '\t\t"""\n'\
+    '\t\t查询结果校验（包括跳转）\n'\
+    '\t\t:param para:\n'\
+    '\t\t"""\n'\
+    '\t\t# self.assertTrue(AssertResult(self).check_query_result(para))\n'\
+    '    def assert_query_criteria(self, para):\n'\
+    '\t\t"""\n'\
+    '\t\t查询条件校验\n'\
+    '\t\t:param para:\n'\
+    '\t\t"""\n'\
+    '\t\t# self.assertTrue(self.check_query_criteria(para))\n\n'\
+    '    @BeautifulReport.add_test_img()\n'\
+    '    @data(*DataAccess.getCaseData({}))\n'\
+    '    def test_query(self, para):\n'\
+    '\t\t"""{}\n'\
+    '\t\t"""\n'\
+    '\t\tself.start_case(para, __file__)\n'\
+    '\t\tself.query(para)\n'\
+    '\t\tself.assert_query_result(para)\n'\
+    '\t\tself.end_case()\n\n'\
+    '    @BeautifulReport.add_test_img()\n'\
+    '    @data(*DataAccess.getCaseData({}, valCheck=True))\n'\
+    '    def _test_checkValue(self, para):\n'\
+    '\t\tself.start_case(para, __file__)\n'\
+    '\t\tself.query(para)\n'\
+    '\t\tself.assert_query_criteria(para)\n'\
+    '\t\tself.end_case()\n'
     str_new = str.format(para,menuPath,para,para)
     return str_new
 
@@ -202,7 +202,7 @@ def  getTestCase(menuNO,tab='01',path ='',name='',para=''):
   for a,b,c,d in data:
 
       str  = b.lower()
-      lis.append('\t#'+d+'\n\t'+dic[c].format(str,b)+'\n\n')
+      lis.append('\t\t#'+d+'\n\t\t'+dic[c].format(str,b)+'\n\n')
   lis.append('\n\n'+testTail(para,getPath(menuNO)))
   ph = path+'/'+name+'.py'
   with open(ph,'w',encoding='utf-8') as file:

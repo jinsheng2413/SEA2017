1e7bc4d3460c0c61c41c598abef71fa798253c49
oslakesx <oslake@qq.com>
2019-03-28 11:24:11 +0800

    供电单位跳转到所优化判断

--- src/com/nrtest/common/assert_result.py
+++ src/com/nrtest/common/assert_result.py
@@ -549,10 +549,12 @@ class AssertResult():
 
                         # 跳转后的相关判断处理
                         after_text = skip_info['AFTER_TEXT']
+                        org_type = DataAccess.get_org_type(after_text)
 
                         # AFTER_ACTION：01-没查询结果；02-查询结果有链接；03-有查询结果，但没链接；
                         after_action = skip_info['AFTER_ACTION']
-                        if after_text.endswith('所') or after_action == '03':  # 跳转后没值或到供电所时，停止跳转
+                        # if after_text.endswith('所') or after_action == '03':  # 跳转后没值或到供电所时，停止跳转
+                        if org_type > '04' or after_action == '03':  # 跳转后没值或到供电所时，停止跳转
                             break
                         elif after_action == '01':  # 校验列没有链接；
                             raise AssertError('“{}”没查询结果，无法继续下钻，请检查是否合理！'.format(link_text))

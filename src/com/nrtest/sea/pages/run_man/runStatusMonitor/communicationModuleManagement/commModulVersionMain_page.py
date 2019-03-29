a090091594dc8454cf9a3b25399c2890e1ae7b2c
luyanyan <luyanyan@qq.com>
2019-03-29 17:04:38 +0800

    代码完善

--- src/com/nrtest/sea/pages/run_man/runStatusMonitor/communicationModuleManagement/commModulVersionMain_page.py
+++ src/com/nrtest/sea/pages/run_man/runStatusMonitor/communicationModuleManagement/commModulVersionMain_page.py
@@ -16,21 +16,16 @@ from com.nrtest.common.base_page import Page
 class CommModulVersionMainPage(Page):
     # 终端类型
     def inputSel_tmnl_type(self, options):
-        # self.click(CommModulVersionMainLocators.QRY_TMNL_TYPE)
-        # locator = self.get_select_locator(CommModulVersionMainLocators.QRY_TMNL_TYPE_VALUE, name)
-        # self.click(locator)
-        self.selectDropDown(options)
+        self.selectCheckBox(options)
+        # self.selectDropDown(options)
 
     # 终端厂商
     def inputSel_tmnl_factory(self, options):
-        # self.click(CommModulVersionMainLocators.QRY_TMNL_FACTORY)
-        # locator = self.get_select_locator(CommModulVersionMainLocators.QRY_TMNL_FACTORY_VALUE, name)
-        # self.click(locator)
         self.selectDropDown(options)
 
     # 终端地址
     def inputStr_tmnl_addr(self, value):
-        self.input(value)#, *CommModulVersionMainLocators.QRY_TMNL_ADDR)
+        self.input(value)
 
     # 终端规约
     def inputSel_tmnl_protocol(self, option):
@@ -38,5 +33,4 @@ class CommModulVersionMainPage(Page):
 
     # 查询
     def btn_qry(self):
-        # self.click(CommModulVersionMainLocators.BTN_QRY)
         self.btn_query()

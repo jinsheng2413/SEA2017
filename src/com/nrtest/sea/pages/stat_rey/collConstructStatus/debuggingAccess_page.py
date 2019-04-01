08cfc433be4ceba59df68f8a3ffbfb9b1f624994
js <543045780@qq.com>
2019-04-01 14:31:49 +0800

    Merge branch 'dev' of http://192.168.176.201:10010/r/project/SEA2017 into dev
    # Conflicts:
    #	src/com/nrtest/setup/nari_test.conf

--- src/com/nrtest/sea/pages/stat_rey/collConstructStatus/debuggingAccess_page.py
+++ src/com/nrtest/sea/pages/stat_rey/collConstructStatus/debuggingAccess_page.py
@@ -41,6 +41,6 @@ class DebuggingAccessPage(Page):
         self.inputDate(content)
 
     # 查询按钮
-    def btn_search(self):
+    def btn_qry(self):
         # self.click(DebuggingAccessLocators.BTN_SEARCH)
         self.btn_query()

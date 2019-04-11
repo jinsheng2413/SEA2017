07dcfefbeb847c8507217cc3ce191e1f2a995329
jinsheng <543045780@qq.com>
2019-04-11 15:04:59 +0800

    基本应用→档案管理→HPLC芯片管理→台户关系识别:台户关系统计 基本应用→档案管理→HPLC芯片管理→台户关系识别:台户关系明细

--- src/com/nrtest/sea/data/base_app/archivesMan/hplcChipMan/hplcChipMan_data.py
+++ src/com/nrtest/sea/data/base_app/archivesMan/hplcChipMan/hplcChipMan_data.py
@@ -14,3 +14,8 @@ class HPLCChipMan_data:
     powerCutAnalyse_para = '99911D40'
     powerCutAnalyse_tabName_stat = '停电统计'
     powerCutAnalyse_tabName_detail = '停电明细'
+
+    # 基本应用→档案管理→HPLC芯片管理→台户关系识别
+    tgRelationIdent_para = '99911D50'
+    tgRelationIdent_tabName_stat = '台户关系统计'
+    tgRelationIdent_tabName_detail = '台户关系明细'

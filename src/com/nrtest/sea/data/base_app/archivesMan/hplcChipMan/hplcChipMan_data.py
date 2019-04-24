d0353ed072f650807d8048de97aa3b1c91982d1d
hx <hx@163.com>
2019-04-24 17:11:22 +0800

    # 基本应用→档案管理→HPLC管理→台户关系识别:台区自动识别

--- src/com/nrtest/sea/data/base_app/archivesMan/hplcChipMan/hplcChipMan_data.py
+++ src/com/nrtest/sea/data/base_app/archivesMan/hplcChipMan/hplcChipMan_data.py
@@ -17,6 +17,7 @@ class HPLCChipMan_data:
 
     # 基本应用→档案管理→HPLC芯片管理→台户关系识别
     tgRelationIdent_para = '99911D50'
+    tgRelationIdent_tabName_auto  = '台区自动识别'
     tgRelationIdent_tabName_stat = '台区关系统计'
     tgRelationIdent_tabName_detail = '台户关系明细'
 
@@ -31,7 +32,6 @@ class HPLCChipMan_data:
     impedanceIdent_para = '99911D70'
     # 基本应用→档案管理→HPLC芯片管理→芯片档案查询功能
     chipArchivesQuery_para = '99911D10'
-    chipArchivesQuery_tab_count = 'HPLC芯片档案统计'
 
     # 基本应用→档案管理→HPLC芯片管理→相位识别
     phaseIdent_para = '99911D60'

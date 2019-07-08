8b36942ae83eabc37d1486ad70ba4aa0f07a1865
guochunbiao <guochunbiao@163.com>
2019-07-08 10:20:25 +0800

    网损管理：分区模型

---
+++ src/com/nrtest/pbs/data/net_loss_man/netLossMan_data.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+"""
+@author: 郭春彪
+@license: (C) Copyright 2018, Nari.
+@file: netLossMan_data.py
+@time: 2019/7/8 0008 9:05
+@desc:
+"""
+class NetLossMan_data:
+    # 网损管理→分区模型
+    partitionModel_para = '00001301'
